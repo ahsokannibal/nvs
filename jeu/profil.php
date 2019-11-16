4d3b3e605f43ed718f7c6743fdafaca051137ddc
loka <romain.perruchon@gmail.com>
2019-11-16 23:20:49 +0100

    * profil * sac * css et images onglet profil

--- jeu/profil.php
+++ jeu/profil.php
@@ -1,14 +1,15 @@
 <?php
 session_start();
 require_once("../fonctions.php");
-db_connexion();
+
+$mysqli = db_connexion();
 
 include ('../nb_online.php');
 
-// recupération config jeu
+// recupÃ©ration config jeu
 $sql = "SELECT disponible FROM config_jeu";
-$res = exec_requete($sql, __LINE__, __FILE__);
-$t_dispo = mysql_fetch_assoc($res);
+$res = $mysqli->query($sql);
+$t_dispo = $res->fetch_assoc();
 $dispo = $t_dispo["disponible"];
 
 if($dispo){
@@ -16,8 +17,8 @@ if($dispo){
 	?>
 	<html>
 	<head>
-	  <title>NAOnline</title>
-	  <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+	  <title>Nord VS Sud</title>
+	  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	   <meta http-equiv="Content-Language" content="fr" />
 	  <link rel="stylesheet" type="text/css" media="screen" href="onglet.css" title="Version 1" />
 	
@@ -26,20 +27,20 @@ if($dispo){
 	<div id="header">
 	  <ul>
 		<li id="current"><a href="#">Profil</a></li>
-		<li><a href="ameliorer.php">Améliorer son perso</a></li>
+		<li><a href="ameliorer.php">AmÃ©liorer son perso</a></li>
 		<li><a href="equipement.php">Equiper son perso</a></li>
-		<li><a href="pnjdex.php">PNJ Dex</a></li>
-		<li><a href="mes_monstres.php">Mes monstres</a></li>
-		<li><a href="compte.php">Gérer son Compte</a></li>
+		<li><a href="compte.php">GÃ©rer son Compte</a></li>
 	  </ul>
 	</div>
 	<?php
 	if (@$_SESSION["id_perso"]) {
+		
 		//recuperation des variables de sessions
 		$id = $_SESSION["id_perso"];
 		$sql = "SELECT pv_perso, est_gele FROM perso WHERE id_perso='$id'";
-		$res = exec_requete($sql, __LINE__, __FILE__);
-		$tpv = mysql_fetch_assoc($res);
+		$res = $mysqli->query($sql);
+		$tpv = $res->fetch_assoc();
+		
 		$testpv = $tpv['pv_perso'];
 		$e_g = $tpv['est_gele'];
 		
@@ -50,71 +51,58 @@ if($dispo){
 		else {
 			
 			if ($testpv <= 0) {
-				echo "<font color=red>Vous êtes mort...</font>";
+				echo "<font color=red>Vous Ãªtes mort...</font>";
 			}
 			else {			
 				// recuperation des infos du perso
-				$sql = "SELECT nom_perso, image_perso, niveau_perso, xp_perso, x_perso, y_perso, deAttaque_perso, deDefense_perso, pm_perso, pi_perso, pv_perso, pvMax_perso, pmMax_perso, pa_perso, paMax_perso, recup_perso, bonusRecup_perso, perception_perso, bonusPerception_perso, bonus_perso, charge_perso, chargeMax_perso, message_perso, description_perso, dateCreation_perso, clan, mainPrincipale_perso FROM perso WHERE id_perso='$id'";
-				$res = exec_requete($sql);
-				$t_i = mysql_fetch_assoc($res);
-				$nom_p = $t_i["nom_perso"];
-				$image_p = $t_i["image_perso"];
-				$xp_p = $t_i["xp_perso"];
-				$x_p = $t_i["x_perso"];
-				$y_p = $t_i["y_perso"];
-				$deA_p = $t_i["deAttaque_perso"];
-				$deD_p = $t_i["deDefense_perso"];
-				$pm_p = $t_i["pm_perso"];
-				$pmM_p = $t_i["pmMax_perso"];
-				$pi_p = $t_i["pi_perso"];
-				$pv_p = $t_i["pv_perso"];
-				$pvM_p = $t_i["pvMax_perso"];
-				$pa_p = $t_i["pa_perso"];
-				$paM_p = $t_i["paMax_perso"];
-				$rec_p = $t_i["recup_perso"];
-				$br_p = $t_i["bonusRecup_perso"];
-				$per_p = $t_i["perception_perso"];
-				$bp_p = $t_i["bonusPerception_perso"];
-				$br_p = $t_i["bonusRecup_perso"];
-				$b_p = $t_i["bonus_perso"];
-				$ch_p = $t_i["charge_perso"];
-				$chM_p = $t_i["chargeMax_perso"];
-				$lvl_p = $t_i["niveau_perso"];
-				$dc_p = $t_i["dateCreation_perso"];
+				$sql = "SELECT nom_perso, image_perso, niveau_perso, xp_perso, pc_perso, x_perso, y_perso, deAttaque_perso, deDefense_perso, pm_perso, pi_perso, pv_perso, pvMax_perso, pmMax_perso, pa_perso, paMax_perso, recup_perso, bonusRecup_perso, perception_perso, bonusPerception_perso, bonus_perso, charge_perso, chargeMax_perso, message_perso, description_perso, dateCreation_perso, clan FROM perso WHERE id_perso='$id'";
+				$res = $mysqli->query($sql);
+				$t_i = $res->fetch_assoc();
+				
+				$nom_p 		= $t_i["nom_perso"];
+				$image_p 	= $t_i["image_perso"];
+				$xp_p 		= $t_i["xp_perso"];
+				$pc_p 		= $t_i["pc_perso"];
+				$x_p 		= $t_i["x_perso"];
+				$y_p 		= $t_i["y_perso"];
+				$deA_p 		= $t_i["deAttaque_perso"];
+				$deD_p 		= $t_i["deDefense_perso"];
+				$pm_p 		= $t_i["pm_perso"];
+				$pmM_p 		= $t_i["pmMax_perso"];
+				$pi_p 		= $t_i["pi_perso"];
+				$pv_p 		= $t_i["pv_perso"];
+				$pvM_p 		= $t_i["pvMax_perso"];
+				$pa_p 		= $t_i["pa_perso"];
+				$paM_p 		= $t_i["paMax_perso"];
+				$rec_p 		= $t_i["recup_perso"];
+				$br_p 		= $t_i["bonusRecup_perso"];
+				$per_p 		= $t_i["perception_perso"];
+				$bp_p 		= $t_i["bonusPerception_perso"];
+				$br_p 		= $t_i["bonusRecup_perso"];
+				$b_p 		= $t_i["bonus_perso"];
+				$ch_p 		= $t_i["charge_perso"];
+				$chM_p 		= $t_i["chargeMax_perso"];
+				$lvl_p 		= $t_i["niveau_perso"];
+				$dc_p 		= $t_i["dateCreation_perso"];
 				$clan_perso = $t_i["clan"];
+				
 				if($clan_perso == '1'){
 					$couleur_clan_perso = 'blue';
-					$nom_clan = 'Bleus';
+					$nom_clan = 'Nord';
 				}
 				if($clan_perso == '2'){
 					$couleur_clan_perso = 'red';
-					$nom_clan = 'Rouges';
+					$nom_clan = 'Sud';
 				}
 				if($clan_perso == '3'){
 					$couleur_clan_perso = 'green';
-					$nom_clan = 'Verts';
+					$nom_clan = 'Indiens';
 				}
 				
 				$mes_p = $t_i["message_perso"];
 				$des_p = $t_i["description_perso"];
 				
-				$mainPrincipale_p = $t_i["mainPrincipale_perso"];
-				
-				$t_im = explode("t",$image_p);
-				$im_p = $t_im[0]."e.png";
-				
-				if(isset($_POST["changer_main_p"]) && $_POST["changer_main_p"] != ""){
-					if($mainPrincipale_p == 1){
-						$new_main_p = 0;
-						$mainPrincipale_p = $new_main_p;
-					}
-					else {
-						$new_main_p = 1;
-						$mainPrincipale_p = $new_main_p;
-					}
-					$sql = "UPDATE perso SET mainPrincipale_perso='$new_main_p' WHERE id_perso='$id'";
-					exec_requete($sql);
-				}
+				$im_p = $nom_clan.".gif";
 				
 		?>
 		<br /><br /><br /><br />
@@ -141,13 +129,13 @@ if($dispo){
 						<td><?php echo "<u><b>Pseudo :</b></u><font color=\"$couleur_clan_perso\"> ".$nom_p." </font>- <b><u>Niveau :</u></b> ".$lvl_p." - <b><u>Camp :</u></b><font color=\"$couleur_clan_perso\"> ".$nom_clan." </font>"; ?></td>
 					</tr>
 					<tr>
-						<td><?php echo "<u><b>Xp :</b></u> ".$xp_p." - <u><b>Pi :</b></u> ".$pi_p.""; ?></td>
+						<td><?php echo "<u><b>Xp :</b></u> ".$xp_p." - <u><b>Pi :</b></u> ".$pi_p." - <u><b>PC :</b></u> ".$pc_p.""; ?></td>
 					</tr>
 					<tr>
 						<td><?php echo "<u><b>Position sur la carte :</b></u> ".$x_p."/".$y_p; ?></td>
 					</tr>
 					<tr>
-						<td><?php echo "<u><b>Nombre de dés :</b></u> ".max($deA_p,$deD_p)." - <b><u>Dégâts :</u></b> ".$deg_p; ?></td>
+						<td><?php echo "<u><b>Nombre de dÃ©s :</b></u> ".max($deA_p,$deD_p).""; ?></td>
 					</tr>
 					<tr>
 						<td><?php echo "<u><b>Mouvements restants :</b></u> ".$pm_p."/".$pmM_p; ?><?php echo " - <u><b>Points de vie :</b></u> ".$pv_p."/".$pvM_p; ?></td>
@@ -156,10 +144,7 @@ if($dispo){
 						<td><?php echo "<u><b>Points d'action :</b></u> ".$pa_p."/".$paM_p." - <u><b>Malus de defense :</b></u> "; if($b_p < 0) echo "<font color=red>"; echo $b_p; ?></td>
 					</tr>
 					<tr>
-						<td><?php echo "<u><b>Récupération :</b></u> ".$rec_p; if($br_p) echo " <font color='blue'>(+".$br_p.")</font>"; ?><?php echo " - <u><b>Perception :</b></u> ".$per_p; if($bp_p) {if($bp_p>0) echo " (+".$bp_p.")"; else echo " (".$bp_p.")";} ?></td>
-					</tr>
-					<tr>
-						<td><?php echo "<u><b>Main principale :</b></u> "; if($mainPrincipale_p == 1){ echo "Droite";} if($mainPrincipale_p == 0){ echo "Gauche";}?>&nbsp;<form method="post" action="profil.php"><input type="submit" name="changer_main_p" value="changer de main"></form></td>
+						<td><?php echo "<u><b>RÃ©cupÃ©ration :</b></u> ".$rec_p; if($br_p) echo " <font color='blue'>(+".$br_p.")</font>"; ?><?php echo " - <u><b>Perception :</b></u> ".$per_p; if($bp_p) {if($bp_p>0) echo " (+".$bp_p.")"; else echo " (".$bp_p.")";} ?></td>
 					</tr>
 				</table>
 				</td>
@@ -183,7 +168,7 @@ if($dispo){
 		}
 	}
 	else{
-		echo "<font color=red>Vous ne pouvez pas accéder à cette page, veuillez vous loguer.</font>";
+		echo "<font color=red>Vous ne pouvez pas accÃ©der Ã  cette page, veuillez vous loguer.</font>";
 	}
 	?>
 	</body>
@@ -192,8 +177,8 @@ if($dispo){
 }
 else {
 	// logout
-	$_SESSION = array(); // On écrase le tableau de session
-	session_destroy(); // On détruit la session
+	$_SESSION = array(); // On Ã©crase le tableau de session
+	session_destroy(); // On dÃ©truit la session
 	
 	header("Location: index2.php");
 }
--- jeu/sac.php
+++ jeu/sac.php
@@ -1,49 +1,57 @@
 <?php
 session_start();
 require_once("../fonctions.php");
-db_connexion();
+
+$mysqli = db_connexion();
 
 include ('../nb_online.php');
 
-// recupération config jeu
+// recuperation config jeu
 $sql = "SELECT disponible FROM config_jeu";
-$res = exec_requete($sql, __LINE__, __FILE__);
-$t_dispo = mysql_fetch_assoc($res);
+$res = $mysqli->query($sql);
+$t_dispo = $res->fetch_assoc();
+
 $dispo = $t_dispo["disponible"];
 
 if($dispo){
 
 	if (@$_SESSION["id_perso"]) {
+		
 		//recuperation des variables de sessions
 		$id = $_SESSION["id_perso"];
 		
-		// recupération des données sur le perso
+		// recuperation des donnees sur le perso
 		$sql = "SELECT pv_perso, pa_perso FROM perso WHERE ID_perso='$id'";
-		$res = exec_requete($sql, __LINE__, __FILE__);
-		$tpv = mysql_fetch_assoc($res);
+		$res = $mysqli->query($sql);
+		$tpv = $res->fetch_assoc();
+		
 		$testpv = $tpv['pv_perso'];
 		$testpa = $tpv['pa_perso'];
 		
 		// On verifie que le perso soit toujours vivant
 		if ($testpv <= 0) {
-			echo "<font color=red>Vous êtes mort...</font>";
+			echo "<font color=red>Vous Ãªtes mort...</font>";
 		}
 		else {
 			// on souhaite utiliser un objet
 			if(isset($_GET["id_obj"]) && $_GET["id_obj"] != ""){
+				
 				// On recupere l'identifiant de l'objet
 				$id_o = $_GET["id_obj"];
 				
 				// On verifie que l'identifiant soit bien un nombre positif
-				$verif = ereg("^[0-9]*[0-9]$","$id_o");
+				$verif = preg_match("#^[0-9]*[0-9]$#i","$id_o");
+				
 				if($verif && $id_o > 0) {
+					
 					// On verifie que l'objet soit bien utilisable
 					if($id_o != 6 && $id_o != 7 && $id_o != 8 && $id_o != 10){
 						// ok
 						//verification que le perso possede bien cet objet
 						$sql = "SELECT id_objet FROM perso_as_objet WHERE id_perso='$id' AND id_objet='$id_o'";
-						$res = exec_requete($sql);
-						$ok = mysql_num_rows($res);
+						$res = $mysqli->query($sql);
+						$ok = $res->num_rows;
+						
 						if($ok) { // possede plus de 0 objets
 							
 							// On verifie que le perso possede bien 1 pa pour utiliser l'objet
@@ -51,8 +59,9 @@ if($dispo){
 							
 								// recuperation des effets de l'objet
 								$sql = "SELECT nom_objet, bonusPerception_objet, bonusRecup_objet, bonusPv_objet, bonusPm_objet, coutPa_objet, poids_objet, type_objet FROM objet WHERE id_objet='$id_o'";
-								$res = exec_requete($sql);
-								$bonus_o = mysql_fetch_assoc($res);
+								$res = $mysqli->query($sql);
+								$bonus_o = $res->fetch_assoc();
+								
 								$nom_ob = $bonus_o["nom_objet"];
 								$bonusPerception = $bonus_o["bonusPerception_objet"];
 								$bonusRecup = $bonus_o["bonusRecup_objet"];
@@ -64,12 +73,13 @@ if($dispo){
 										
 								// on supprime l'objet de l'inventaire
 								$sql = "DELETE FROM perso_as_objet WHERE id_perso='$id' AND id_objet='$id_o' LIMIT 1";
-								exec_requete($sql);
+								$mysqli->query($sql);
 										
-								// on recupere les pv et autres données du perso
+								// on recupere les pv et autres donnees du perso
 								$sql = "SELECT pv_perso, pvMax_perso, recup_perso, bonusRecup_perso FROM perso WHERE id_perso='$id'";
-								$res = exec_requete($sql);
-								$t_p = mysql_fetch_assoc($res);
+								$res = $mysqli->query($sql);
+								$t_p = $res->fetch_assoc();
+								
 								$pv_p = $t_p["pv_perso"];
 								$pvM_p = $t_p["pvMax_perso"];
 								$rec_p = $t_p["recup_perso"];
@@ -78,33 +88,37 @@ if($dispo){
 								// si l'objet donne des pv/pm/perception/recup
 								if($bonusPv || $bonusPerception || $bonusPm || $bonusRecup) { 
 										
-									// l'objet lui fait recuperer un nombre de pv tel que ses pv après soient inferieurs à ses pvmax 
-									if($pv_p + $bonusPv < $pvM_p){ 
+									// l'objet lui fait recuperer un nombre de pv tel que ses pv apres soient inferieurs a ses pvmax 
+									if($pv_p + $bonusPv < $pvM_p){
+										
 										// on applique les effets de l'objet sur le perso
 										$sql = "UPDATE perso 
 												SET pa_perso=pa_perso-1, pv_perso=pv_perso+$bonusPv, bonusPerception_perso=bonusPerception_perso+$bonusPerception, pm_perso=pm_perso+$bonusPm, bonusRecup_perso=bonusRecup_perso+$bonusRecup
 												WHERE id_perso='$id'";
-										exec_requete($sql);
+										$mysqli->query($sql);
 										
 										// Affichage 
-										echo "Vous avez utilisé ".$nom_ob."<br>";
-										if($type_o == 'S' || $type_o == 'SSP' || $id_o == 4)
+										echo "Vous avez utilisÃ© ".$nom_ob."<br>";
+										if($type_o == 'S' || $type_o == 'SSP' || $id_o == 4) {
 											echo "Vous gagnez ".$bonusPv." pv<br>";
+										}
 									}
 									else { // l'effet de l'objet lui rend des pv tel qu'il atteind son max de pv (ou superieur mais on ne peut pas depasser le max de pv) 
+									
 										// on applique les effets de l'objet sur le perso
 										$sql = "UPDATE perso 
 												SET pv_perso=pvMax_perso, bonusPerception_perso=bonusPerception_perso+$bonusPerception, pm_perso=pm_perso+$bonusPm, bonusRecup_perso=bonusRecup_perso+$bonusRecup 
 												WHERE id_perso='$id'";
-										exec_requete($sql);
+										$mysqli->query($sql);
 										
 										// calcul du gain de pv
 										$gain_pv = $pvM_p - $pv_p;
 											
 										// Affichage 
-										echo "Vous avez utilisé ".$nom_ob."<br>";
-										if($type_o == 'S' || $type_o == 'SSP' || $id_o == 4)
+										echo "Vous avez utilisÃ© ".$nom_ob."<br>";
+										if($type_o == 'S' || $type_o == 'SSP' || $id_o == 4) {
 											echo "Vous gagnez ".$gain_pv." pv (Vous avez atteind votre maximum de points de vie)<br>";
+										}
 									}
 								}
 								
@@ -112,14 +126,14 @@ if($dispo){
 								if($id_o == 4){
 									// le perso est bourre
 									$sql = "UPDATE perso SET bourre_perso=bourre_perso+1 WHERE id_perso='$id'";
-									exec_requete($sql);
+									$mysqli->query($sql);
 									
 									// calcul du gain de recup
 									$recup_i = $rec_p+$br_p;
 									
 									// Affichage 
-									echo "Votre recuperation passe de ".$recup_i." à ";
-									echo $rec_p+$br_p+$bonusRecup." mais il parait que l'abus d'alcool est dangereux pour la santé... (perception -2)<br>";
+									echo "Votre recuperation passe de ".$recup_i." Ã  ";
+									echo $rec_p+$br_p+$bonusRecup." mais il parait que l'abus d'alcool est dangereux pour la santÃ©... (perception -2)<br>";
 								}
 								
 								// Longue vue
@@ -130,9 +144,9 @@ if($dispo){
 								
 								// Fiole du berserker
 								if($id_o == 9){
-									// on enlève les malus de combat au perso
+									// on enleve les malus de combat au perso
 									$sql = "UPDATE perso SET bonus_perso='0' WHERE id_perso='$id'";
-									exec_requete($sql);									
+									$mysqli->query($sql);									
 									
 									// Affichage
 									echo "Vous ne ressentez plus aucune douleurs et vous avez l'impression de pouvoir vous mouvoir comme il vous semble<br>";
@@ -140,14 +154,14 @@ if($dispo){
 								
 								// MAJ charge perso
 								$sql_c = "UPDATE perso SET charge_perso=charge_perso-$poids WHERE id_perso='$id'";
-								exec_requete($sql_c);
+								$mysqli->query($sql_c);
 							}
 							else {
 								echo "Vous n'avez pas assez de PA, l'utilisation d'un objet coute 1 PA.";
 							}
 						}
 						else {
-							echo "Vous ne possédez pas/plus cet objet...";
+							echo "Vous ne possÃ©dez pas/plus cet objet...";
 						}
 					}
 					else {
@@ -161,7 +175,7 @@ if($dispo){
 		?>
 	<html>
 	<head>
-	<title>NAOnline</title>
+	<title>Nord VS Sud</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
 	<link href="../style.css" rel="stylesheet" type="text/css">
 	</head>
@@ -170,14 +184,16 @@ if($dispo){
 	<?php
 			// recuperation du nombre d'objet que possede le perso
 			$sql = "SELECT COUNT(id_objet) FROM perso_as_objet WHERE id_perso='$id'";
-			$res = exec_requete($sql);
-			$t_nb_objets = mysql_fetch_row($res);
+			$res = $mysqli->query($sql);
+			$t_nb_objets = $res->fetch_row();
+			
 			$nb_objets = $t_nb_objets[0];
 			
 			// recuperation de l'or que possede le perso
 			$sql = "SELECT or_perso, charge_perso, chargeMax_perso FROM perso WHERE id_perso='$id'";
-			$res = exec_requete($sql);
-			$t_or = mysql_fetch_assoc($res);
+			$res = $mysqli->query($sql);
+			$t_or = $res->fetch_assoc();
+			
 			$or_p = $t_or["or_perso"];
 			$charge_perso = $t_or["charge_perso"];
 			$chargeMax_perso = $t_or["chargeMax_perso"];
@@ -190,7 +206,7 @@ if($dispo){
 				<td align=center width=25%><img src="../images/sac.png"><p align="center"><input type="button" value="Fermer mon sac" onclick="window.close()"></p></td>
 				<td width=75%>
 					<center><h2>Mon sac</h2>
-					<p>Le sac vous permet de transporter des objets et de les utiliser.<br>Vous possédez <b><?php echo $nb_objets; ?></b> objet<?php if($nb_objets > 1){echo "s";} ?> dans votre sac.</p>
+					<p>Le sac vous permet de transporter des objets et de les utiliser.<br>Vous possÃ©dez <b><?php echo $nb_objets; ?></b> objet<?php if($nb_objets > 1){echo "s";} ?> dans votre sac.</p>
 					<?php 
 					echo "<p><u><b>Charge :</b></u> ";
 					if($charge_perso > $chargeMax_reel){
@@ -201,7 +217,7 @@ if($dispo){
 					}
 					echo "".$charge_perso."</font> / ".$chargeMax_reel."</p>"; 
 					?>
-					<img src="../images/pierre_t.png" align="middle">Vous possédez <b><?php echo $or_p; ?></b> pièce<?php if($or_p > 1){echo "s";}?> d'or.<br>
+					<img src="../images/pierre_t.png" align="middle">Vous possÃ©dez <b><?php echo $or_p; ?></b> piÃ©ce<?php if($or_p > 1){echo "s";}?> d'or.<br>
 					</center>
 				</td>
 			</tr>
@@ -215,25 +231,27 @@ if($dispo){
 			
 			// recuperation du nombre de type d'objets que possede le perso
 			$sql = "SELECT DISTINCT id_objet FROM perso_as_objet WHERE id_perso='$id'";
-			$res = exec_requete($sql);
-			$nb_obj = mysql_num_rows($res);
-			while ($t_obj = mysql_fetch_assoc($res)){
+			$res = $mysqli->query($sql);
+			$nb_obj = $res->num_rows;
+			
+			while ($t_obj = $res->fetch_assoc()){
 			
 					// id de l'objet
 					$id_obj = $t_obj["id_objet"];
 					
 					// recuperation des carac de l'objet
 					$sql1 = "SELECT nom_objet, poids_objet, description_objet FROM objet WHERE id_objet='$id_obj'";
-					$res1 = exec_requete($sql1);
-					$t_o = mysql_fetch_assoc($res1);
+					$res1 = $mysqli->query($sql1);
+					$t_o = $resl->fetch_assoc();
+					
 					$nom_o = $t_o["nom_objet"];
 					$poids_o = $t_o["poids_objet"];
 					$description_o = $t_o["description_objet"];
 					
 					// recuperation du nombre d'objet de ce type que possede le perso
 					$sql2 = "SELECT id_objet FROM perso_as_objet WHERE id_perso='$id' AND id_objet='$id_obj'";
-					$res2 = exec_requete($sql2);
-					$nb_o = mysql_num_rows($res2);
+					$res2 = $mysqli->query($sql2);
+					$nb_o = $res2->num_rows;
 					
 					// calcul poids
 					$poids_total_o = $poids_o * $nb_o;
@@ -242,7 +260,7 @@ if($dispo){
 					echo "<tr>";
 					echo "<td align='center'><img src=\"../images/objet".$id_obj.".png\"></td>";
 					echo "<td align='center'><font color=green><b>".$nom_o."</b></font><br>".stripslashes($description_o)."</td>";
-					echo "<td align='center'>Vous possédez <b>".$nb_o."</b> ".$nom_o."";
+					echo "<td align='center'>Vous possÃ©dez <b>".$nb_o."</b> ".$nom_o."";
 					if($nb_o > 1 && $id_obj != 6 && $id_obj != 7){ 
 						echo "s";
 					}
@@ -264,15 +282,15 @@ if($dispo){
 		}
 	}
 	else{
-		echo "<font color=red>Vous ne pouvez pas accéder à cette page, veuillez vous loguer.</font>";
+		echo "<font color=red>Vous ne pouvez pas accÃ©der Ã  cette page, veuillez vous loguer.</font>";
 	}
 	?>
 <?php
 }
 else {
 	// logout
-	$_SESSION = array(); // On écrase le tableau de session
-	session_destroy(); // On détruit la session
+	$_SESSION = array(); // On ecrase le tableau de session
+	session_destroy(); // On detruit la session
 	
 	header("Location: index2.php");
 }
