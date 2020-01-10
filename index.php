5da9fbd58bceaa40510c68c7e08216f1e7739dcb
loka <romain.perruchon@gmail.com>
2020-01-10 22:12:55 +0100

    Maj Page index

--- index.php
+++ index.php
@@ -66,7 +66,10 @@ $count_online = $res->fetch_array();
 		<div class="container-fluid">
 			
 			<div class="row">
-				<div class="col-12">
+				<div class="col-4">
+					<img src="images/accueil/logo_NVS_lee.png" alt='baniere NVS' class="img-fluid" />
+				</div>
+				<div class="col-8">
 					<div align='center'><img src="images/accueil/baniere_accueil.jpg" alt='baniere NVS' class="img-fluid" /></div>
 				</div>
 			</div>
