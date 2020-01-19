c7370c68564fd7866c8abeda3213eead7b90dff0
loka <romain.perruchon@gmail.com>
2020-01-19 13:44:09 +0100

    Ajout bouton don paypal

--- index.php
+++ index.php
@@ -97,6 +97,13 @@ $count_online = $res->fetch_array();
 							<a href="faq.php" style="color: white;">FAQ - Régles</b></a>
 							<hr />
 							<a href="http://nordvssud-creation.forumactif.com/" style="color: white;">Le Forum</b></a>
+							<hr />
+							<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+								<input type="hidden" name="cmd" value="_s-xclick" />
+								<input type="hidden" name="hosted_button_id" value="YRKPHY4WX37F6" />
+								<input type="image" src="https://www.paypalobjects.com/fr_FR/FR/i/btn/btn_donateCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Bouton Faites un don avec PayPal" />
+								<img alt="" border="0" src="https://www.paypal.com/fr_FR/i/scr/pixel.gif" width="1" height="1" />
+							</form>
 						</div>
 						
 						<div class="col-8">
