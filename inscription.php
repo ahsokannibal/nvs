356d9acafab17e47139003c852e8cff31aec0467
loka <romain.perruchon@gmail.com>
2020-02-01 00:47:32 +0100

    Correctif nom bataillon avec quote

--- inscription.php
+++ inscription.php
@@ -195,6 +195,8 @@ if(config_dispo_jeu($mysqli)){
 								$lock = "LOCK TABLE (perso) WRITE";
 								$mysqli->query($lock);
 								
+								$nom_bataillon = addslashes($nom_bataillon);
+								
 								$insert_sql = "	INSERT INTO perso (IDJoueur_perso, nom_perso, x_perso, y_perso, pvMax_perso, pv_perso, pm_perso, pmMax_perso, perception_perso, recup_perso, protec_perso, pa_perso, image_perso, dateCreation_perso, DLA_perso, or_perso, clan, message_perso, chef, bataillon) 
 												VALUES ('$IDJoueur_perso','$nom_perso','$x','$y','$pvMax_chef','$pvMax_chef','$pmMax_chef','$pmMax_chef','$perc_chef','$recup_chef','$protec_chef','$pamax_chef','$image_chef',NOW(),FROM_UNIXTIME($dla), '20', $camp, '', 1, '$nom_bataillon')";
 								
