885b476ab532a0f6600ea1717e092c230a07a663
loka <romain.perruchon@gmail.com>
2020-03-21 19:05:47 +0100

    Ajout lien vers page statistiques

--- index.php
+++ index.php
@@ -103,6 +103,8 @@ $count_online = $res->fetch_array();
 							<hr />
 							<a href="jeu/classement.php" style="color: white;">Les classements</b></a>
 							<hr />
+							<a href="jeu/statistiques.php" style="color: white;">Les statistiques</b></a>
+							<hr />
 							<a href="credits.php" style="color: white;">Crédits</b></a>
 							<hr />
 							<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
