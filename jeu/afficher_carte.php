b2d7a04644b7a4754498f3190fb8756be9e7bcee
loka <romain.perruchon@gmail.com>
2020-07-11 14:03:28 +0200

    correctif

--- jeu/afficher_carte.php
+++ jeu/afficher_carte.php
@@ -9,14 +9,6 @@ if (isset($_SESSION["id_perso"])) {
 	
 	$mysqli = db_connexion();
 	
-	/*
-	function setImgDownload($imagePath) {
-		$image = imagecreatefrompng($imagePath);
-		header('Content-Type: image/png');
-		imagepng($image);
-	}
-	*/
-	
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
@@ -60,175 +52,157 @@ if (isset($_SESSION["id_perso"])) {
 	</body>
 </html>	
 	<?php
-	//dans la page qui doit afficher la carte:
-	$requete = $mysqli->query("SELECT * FROM carte_time");
-	$sql = $requete->fetch_array();
-	$timerefresh = $sql['timerefresh'];
-					  
-	$Tpsrestant = Floor(($timerefresh-time())/60);
-					  
-	if ($Tpsrestant <=0)
-	{
-		$timerefresh = time()+60*5;
-		$mysqli->query("UPDATE carte_time Set timerefresh='$timerefresh'") or die (mysql_error());
-		echo 'Mise à jour de l\'historique de la carte, veuillez patienter. Merci....';
-		echo '<meta http-equiv="refresh" content="1;URL=carte.php">';
-	}
-	else {
 		
-		// Le perso appartient-il à une compagnie 
-		$sql = "SELECT id_compagnie from perso_in_compagnie where id_perso='$id' AND (attenteValidation_compagnie='0' OR attenteValidation_compagnie='2')";
-		$res = $mysqli->query($sql);
-		$nb_compagnie = $res->num_rows;		
+	// Le perso appartient-il à une compagnie 
+	$sql = "SELECT id_compagnie from perso_in_compagnie where id_perso='$id' AND (attenteValidation_compagnie='0' OR attenteValidation_compagnie='2')";
+	$res = $mysqli->query($sql);
+	$nb_compagnie = $res->num_rows;		
+	
+	if(isset($_POST['Submit'])){
 		
-		if(isset($_POST['Submit'])){
+		// Enlever le fond
+		if($_POST['Submit'] == "Retirer la topographie"){
 			
-			// Enlever le fond
-			if($_POST['Submit'] == "Retirer la topographie"){
-				
-				echo "<center><h1>Carte Stratégique - sans Topographie</h1></center>";
-				
+			echo "<center><h1>Carte Stratégique - sans Topographie</h1></center>";
+			
+			echo "<center><input type='text' id='idInput' disabled /><br />";
+			echo "<img id='carto' src=\"image_carte.php?imagename=perso$id.png\"></center>"; 
+			echo 'Mise a jour de la carte dans '.$Tpsrestant.'mn.';
+		
+			echo "<div align=\"center\"><br>";
+			echo "Vous pouvez remettre la topographie si vous le souhaitez<br>";
+			echo "<form action=\"afficher_carte.php\" method=\"post\" name=\"avec_fond\">";
+			echo "<input type=\"submit\" name=\"Submit\" value=\"Remettre la topographie\">";
+			echo "</div>";
+			echo "</form>";
+		}
+		else {
+			
+			// Enlever la legende
+			if($_POST['Submit'] == "enlever la legende"){
 				echo "<center><input type='text' id='idInput' disabled /><br />";
-				echo "<img id='carto' src=\"image_carte.php?imagename=perso$id.png\"></center>"; 
+				echo "<img id='carto' src=\"image_carte.php?imagename=carte_sl$id.png\"></center>"; 
 				echo 'Mise a jour de la carte dans '.$Tpsrestant.'mn.';
 			
 				echo "<div align=\"center\"><br>";
-				echo "Vous pouvez remettre la topographie si vous le souhaitez<br>";
+				echo "Vous pouvez remettre la legende si vous le souhaitez<br>";
 				echo "<form action=\"afficher_carte.php\" method=\"post\" name=\"avec_fond\">";
-				echo "<input type=\"submit\" name=\"Submit\" value=\"Remettre la topographie\">";
+				echo "<input type=\"submit\" name=\"Submit\" value=\"remettre la legende\">";
 				echo "</div>";
 				echo "</form>";
 			}
 			else {
 				
-				// Enlever la legende
-				if($_POST['Submit'] == "enlever la legende"){
+				// Remettre la legende
+				if($_POST['Submit'] == "remettre la legende"){
 					echo "<center><input type='text' id='idInput' disabled /><br />";
-					echo "<img id='carto' src=\"image_carte.php?imagename=carte_sl$id.png\"></center>"; 
+					echo "<img id='carto' src=\"image_carte.php?imagename=carte$id.png\"></center>"; 
 					echo 'Mise a jour de la carte dans '.$Tpsrestant.'mn.';
 				
 					echo "<div align=\"center\"><br>";
 					echo "Vous pouvez remettre la legende si vous le souhaitez<br>";
 					echo "<form action=\"afficher_carte.php\" method=\"post\" name=\"avec_fond\">";
-					echo "<input type=\"submit\" name=\"Submit\" value=\"remettre la legende\">";
+					echo "<input type=\"submit\" name=\"Submit\" value=\"Retirer la topographie\">";
 					echo "</div>";
 					echo "</form>";
 				}
 				else {
 					
-					// Remettre la legende
-					if($_POST['Submit'] == "remettre la legende"){
+					if ($_POST['Submit'] == "cercles sur mon bataillon") {
+						
+						echo "<center><h1>Carte Stratégique - Mon bataillon</h1></center>";
+						
+						echo "<center><input type='text' id='idInput' disabled /><br />";
+						echo "<img id='carto' src=\"image_carte.php?imagename=carte_bataillon_sl$id.png\"></center>"; 
+						echo 'Mise a jour de la carte dans '.$Tpsrestant.'mn.';
+					
+						echo "<div align=\"center\"><br>";
+						echo "Vous pouvez enlever la topographie si vous le souhaitez<br>";
+						echo "<form action=\"afficher_carte.php\" method=\"post\" name=\"ss_fond\">";
+						echo "<input type=\"submit\" name=\"Submit\" value=\"Retirer la topographie\"><br />";
+						echo "<input type=\"submit\" name=\"Submit\" value=\"cercles sur mon perso\">";
+						if ($nb_compagnie) {
+							echo "<input type=\"submit\" name=\"Submit\" value=\"cercles sur ma compagnie\">";
+						}
+						echo "</div>";
+						echo "</form>";
+					}
+					else if ($_POST['Submit'] == "cercles sur ma compagnie" && $nb_compagnie) {
+						
+						echo "<center><h1>Carte Stratégique - Ma compagnie</h1></center>";
+						
+						echo "<center><input type='text' id='idInput' disabled /><br />";
+						echo "<img id='carto' src=\"image_carte.php?imagename=carte_compagnie_sl$id.png\"></center>"; 
+						echo 'Mise a jour de la carte dans '.$Tpsrestant.'mn.';
+					
+						echo "<div align=\"center\"><br>";
+						echo "Vous pouvez enlever la topographie si vous le souhaitez<br>";
+						echo "<form action=\"afficher_carte.php\" method=\"post\" name=\"ss_fond\">";
+						echo "<input type=\"submit\" name=\"Submit\" value=\"Retirer la topographie\"><br />";
+						echo "<input type=\"submit\" name=\"Submit\" value=\"cercles sur mon bataillon\">";
+						echo "<input type=\"submit\" name=\"Submit\" value=\"cercles sur mon perso\">";
+						echo "</div>";
+						echo "</form>";
+					}
+					else if ($_POST['Submit'] == "cercles sur mon perso") {
+						
+						echo "<center><h1>Carte Stratégique - Mon perso</h1></center>";							
+							
 						echo "<center><input type='text' id='idInput' disabled /><br />";
 						echo "<img id='carto' src=\"image_carte.php?imagename=carte$id.png\"></center>"; 
 						echo 'Mise a jour de la carte dans '.$Tpsrestant.'mn.';
 					
 						echo "<div align=\"center\"><br>";
-						echo "Vous pouvez remettre la legende si vous le souhaitez<br>";
-						echo "<form action=\"afficher_carte.php\" method=\"post\" name=\"avec_fond\">";
-						echo "<input type=\"submit\" name=\"Submit\" value=\"Retirer la topographie\">";
+						echo "Vous pouvez enlever la topographie si vous le souhaitez<br>";
+						echo "<form action=\"afficher_carte.php\" method=\"post\" name=\"ss_fond\">";
+						echo "<input type=\"submit\" name=\"Submit\" value=\"Retirer la topographie\"><br />";
+						echo "<input type=\"submit\" name=\"Submit\" value=\"cercles sur mon bataillon\">";
+						if ($nb_compagnie) {
+							echo "<input type=\"submit\" name=\"Submit\" value=\"cercles sur ma compagnie\">";
+						}
 						echo "</div>";
 						echo "</form>";
 					}
 					else {
+						echo "<center><h1>Carte Stratégique</h1></center>";
 						
-						if ($_POST['Submit'] == "cercles sur mon bataillon") {
-							
-							echo "<center><h1>Carte Stratégique - Mon bataillon</h1></center>";
-							
-							echo "<center><input type='text' id='idInput' disabled /><br />";
-							echo "<img id='carto' src=\"image_carte.php?imagename=carte_bataillon_sl$id.png\"></center>"; 
-							echo 'Mise a jour de la carte dans '.$Tpsrestant.'mn.';
-						
-							echo "<div align=\"center\"><br>";
-							echo "Vous pouvez enlever la topographie si vous le souhaitez<br>";
-							echo "<form action=\"afficher_carte.php\" method=\"post\" name=\"ss_fond\">";
-							echo "<input type=\"submit\" name=\"Submit\" value=\"Retirer la topographie\"><br />";
-							echo "<input type=\"submit\" name=\"Submit\" value=\"cercles sur mon perso\">";
-							if ($nb_compagnie) {
-								echo "<input type=\"submit\" name=\"Submit\" value=\"cercles sur ma compagnie\">";
-							}
-							echo "</div>";
-							echo "</form>";
+						echo "<center><input type='text' id='idInput' disabled /><br />";
+						echo "<img id='carto' src=\"image_carte.php?imagename=carte$id.png\"></center>"; 
+						echo 'Mise a jour de la carte dans '.$Tpsrestant.'mn.';
+					
+						echo "<div align=\"center\"><br>";
+						echo "Vous pouvez enlever la topographie si vous le souhaitez<br>";
+						echo "<form action=\"afficher_carte.php\" method=\"post\" name=\"ss_fond\">";
+						echo "<input type=\"submit\" name=\"Submit\" value=\"Retirer la topographie\"><br />";
+						echo "<input type=\"submit\" name=\"Submit\" value=\"cercles sur mon bataillon\">";
+						if ($nb_compagnie) {
+							echo "<input type=\"submit\" name=\"Submit\" value=\"cercles sur ma compagnie\">";
 						}
-						else if ($_POST['Submit'] == "cercles sur ma compagnie" && $nb_compagnie) {
-							
-							echo "<center><h1>Carte Stratégique - Ma compagnie</h1></center>";
-							
-							echo "<center><input type='text' id='idInput' disabled /><br />";
-							echo "<img id='carto' src=\"image_carte.php?imagename=carte_compagnie_sl$id.png\"></center>"; 
-							echo 'Mise a jour de la carte dans '.$Tpsrestant.'mn.';
-						
-							echo "<div align=\"center\"><br>";
-							echo "Vous pouvez enlever la topographie si vous le souhaitez<br>";
-							echo "<form action=\"afficher_carte.php\" method=\"post\" name=\"ss_fond\">";
-							echo "<input type=\"submit\" name=\"Submit\" value=\"Retirer la topographie\"><br />";
-							echo "<input type=\"submit\" name=\"Submit\" value=\"cercles sur mon bataillon\">";
-							echo "<input type=\"submit\" name=\"Submit\" value=\"cercles sur mon perso\">";
-							echo "</div>";
-							echo "</form>";
-						}
-						else if ($_POST['Submit'] == "cercles sur mon perso") {
-							
-							echo "<center><h1>Carte Stratégique - Mon perso</h1></center>";							
-								
-							echo "<center><input type='text' id='idInput' disabled /><br />";
-							echo "<img id='carto' src=\"image_carte.php?imagename=carte$id.png\"></center>"; 
-							echo 'Mise a jour de la carte dans '.$Tpsrestant.'mn.';
-						
-							echo "<div align=\"center\"><br>";
-							echo "Vous pouvez enlever la topographie si vous le souhaitez<br>";
-							echo "<form action=\"afficher_carte.php\" method=\"post\" name=\"ss_fond\">";
-							echo "<input type=\"submit\" name=\"Submit\" value=\"Retirer la topographie\"><br />";
-							echo "<input type=\"submit\" name=\"Submit\" value=\"cercles sur mon bataillon\">";
-							if ($nb_compagnie) {
-								echo "<input type=\"submit\" name=\"Submit\" value=\"cercles sur ma compagnie\">";
-							}
-							echo "</div>";
-							echo "</form>";
-						}
-						else {
-							echo "<center><h1>Carte Stratégique</h1></center>";
-							
-							echo "<center><input type='text' id='idInput' disabled /><br />";
-							echo "<img id='carto' src=\"image_carte.php?imagename=carte$id.png\"></center>"; 
-							echo 'Mise a jour de la carte dans '.$Tpsrestant.'mn.';
-						
-							echo "<div align=\"center\"><br>";
-							echo "Vous pouvez enlever la topographie si vous le souhaitez<br>";
-							echo "<form action=\"afficher_carte.php\" method=\"post\" name=\"ss_fond\">";
-							echo "<input type=\"submit\" name=\"Submit\" value=\"Retirer la topographie\"><br />";
-							echo "<input type=\"submit\" name=\"Submit\" value=\"cercles sur mon bataillon\">";
-							if ($nb_compagnie) {
-								echo "<input type=\"submit\" name=\"Submit\" value=\"cercles sur ma compagnie\">";
-							}
-							echo "</div>";
-							echo "</form>";
-						}
+						echo "</div>";
+						echo "</form>";
 					}
 				}
 			}
 		}
-		else {			
-			
-			echo "<center><h1>Carte Stratégique - Mon perso</h1></center>";
+	}
+	else {			
 		
-			echo "<center><input type='text' id='idInput' disabled /><br />";
-			echo "<img id='carto' src=\"image_carte.php?imagename=carte$id.png\"></center>";
-			
-			echo 'Mise a jour de la carte dans '.$Tpsrestant.'mn.';
-		
-			echo "<div align=\"center\"><br>";
-			echo "Vous pouvez enlever la topographie si vous le souhaitez<br>";
-			echo "<form action=\"afficher_carte.php\" method=\"post\" name=\"ss_fond\">";
-			echo "<input type=\"submit\" name=\"Submit\" value=\"Retirer la topographie\"><br />";
-			echo "<input type=\"submit\" name=\"Submit\" value=\"cercles sur mon bataillon\">";
-			if ($nb_compagnie) {
-				echo "<input type=\"submit\" name=\"Submit\" value=\"cercles sur ma compagnie\">";
-			}
-			echo "</div>";
-			echo "</form>";
-			
+		echo "<center><h1>Carte Stratégique - Mon perso</h1></center>";
+	
+		echo "<center><input type='text' id='idInput' disabled /><br />";
+		echo "<img id='carto' src=\"image_carte.php?imagename=carte$id.png\"></center>";
+	
+		echo "<div align=\"center\"><br>";
+		echo "Vous pouvez enlever la topographie si vous le souhaitez<br>";
+		echo "<form action=\"afficher_carte.php\" method=\"post\" name=\"ss_fond\">";
+		echo "<input type=\"submit\" name=\"Submit\" value=\"Retirer la topographie\"><br />";
+		echo "<input type=\"submit\" name=\"Submit\" value=\"cercles sur mon bataillon\">";
+		if ($nb_compagnie) {
+			echo "<input type=\"submit\" name=\"Submit\" value=\"cercles sur ma compagnie\">";
 		}
+		echo "</div>";
+		echo "</form>";
+		
 	}
 }
 else {
