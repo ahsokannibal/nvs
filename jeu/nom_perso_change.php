ae76ea76a79ed4c9dc979547375fbd37ea70adae
loka <romain.perruchon@gmail.com>
2020-08-03 18:01:19 +0200

    Ajout contrôle pour changement de nom chef

--- jeu/nom_perso_change.php
+++ jeu/nom_perso_change.php
@@ -48,31 +48,36 @@ if ($chef) {
 					
 		$nouveau_nom_perso = addslashes($_POST['nouveau_nom_perso']);
 		
-		// Est ce que le nom de cette compagnie est déjà pris ?
-		$sql = "SELECT * FROM perso WHERE nom_perso='$nouveau_nom_perso'";
-		$res = $mysqli->query($sql);
-		$verif = $res->num_rows;
-		
-		if ($verif == 0) {
-			
-			// Existe t-il déjà une demande de changement de nom ?
-			$sql = "SELECT * FROM perso_demande_anim WHERE id_perso='$id' AND type_demande='1'";
-			$res = $mysqli->query($sql);
-			$verif_demande = $res->num_rows;
-			
-			if ($verif_demande == 0) {
-			
-				$sql = "INSERT INTO perso_demande_anim (id_perso, type_demande, info_demande) VALUES ('$id', '1', '$nouveau_nom_perso')";
-				$mysqli->query($sql);
-				
-				echo "<center><font color='blue'>Demande envoyée avec succée</font></center>";
-			}
-			else {
-				echo "<center><font color='red'>Une demande de changement de nom pour ce perso est déjà en attente</font></center>";
-			}
+		if (!filtre($nouveau_nom_perso,1,20) || ctype_digit($nouveau_nom_perso) || strpos($nouveau_nom_perso,'--') !== false) {
+			echo "<center><font color='red'>Le Pseudo est incorrect! Veuillez en choisir un autre (taille entre 1 et 20, pas de quote, pas que des chiffres, pas la chaine --, etc..)</font></center>";
 		}
 		else {
-			echo "<center><font color='red'>Un perso du nom " . $_POST['nouveau_nom_perso'] . " existe déjà, veuillez choisir un autre nom</font></center>";
+			// Est ce que le nom de ce persoe est déjà pris ?
+			$sql = "SELECT * FROM perso WHERE nom_perso='$nouveau_nom_perso'";
+			$res = $mysqli->query($sql);
+			$verif = $res->num_rows;
+			
+			if ($verif == 0) {
+				
+				// Existe t-il déjà une demande de changement de nom ?
+				$sql = "SELECT * FROM perso_demande_anim WHERE id_perso='$id' AND type_demande='1'";
+				$res = $mysqli->query($sql);
+				$verif_demande = $res->num_rows;
+				
+				if ($verif_demande == 0) {
+				
+					$sql = "INSERT INTO perso_demande_anim (id_perso, type_demande, info_demande) VALUES ('$id', '1', '$nouveau_nom_perso')";
+					$mysqli->query($sql);
+					
+					echo "<center><font color='blue'>Demande envoyée avec succée</font></center>";
+				}
+				else {
+					echo "<center><font color='red'>Une demande de changement de nom pour ce perso est déjà en attente</font></center>";
+				}
+			}
+			else {
+				echo "<center><font color='red'>Un perso du nom " . $_POST['nouveau_nom_perso'] . " existe déjà, veuillez choisir un autre nom</font></center>";
+			}
 		}
 	}
 				
