00ff8bc9f72b6ead88c8697e4d17231aba69f298
loka <romain.perruchon@gmail.com>
2020-08-29 20:55:06 +0200

    Suppression de l'icone d'accès à la page des compagnies sur la page de jeu principale si chien

--- jeu/jouer.php
+++ jeu/jouer.php
@@ -2337,6 +2337,8 @@ if($dispo || $admin){
 					$res = $mysqli->query($sql);
 					$nb_compagnie_attente_em = $res->num_rows;
 					
+				} else if ($type_perso == 6) {
+					$pourc_icone = "16%";
 				} else {
 					$pourc_icone = "14%";
 				}
@@ -2408,10 +2410,14 @@ if($dispo || $admin){
 							<td align="center" width=<?php echo $pourc_icone; ?>><a href="carte2.php" target='_blank'><img width=88 height=92 border=0 src="../images/carte2.png" alt="mini map"></a></td>
 							<td align="center" width=<?php echo $pourc_icone; ?>><a href="messagerie.php" target='_blank'><img width=88 height=92 border=0 src="../images/<?php echo $image_messagerie; ?>" alt="messagerie"></a></td>
 							<td align="center" width=<?php echo $pourc_icone; ?>><a href="classement.php" target='_blank'><img width=88 height=92 border=0 src="../images/classement2.png" alt="classement"></a></td>
+							<?php
+							if ($type_perso != 6) {
+							?>
 							<td align="center" width=<?php echo $pourc_icone; ?>>
 								<a href="compagnie.php" target='_blank'><img width=88 height=92 border=0 src="../images/<?php echo $image_compagnie; ?>" alt="compagnie"></a>
 							</td>
 							<?php
+							}
 							if ($nb_em) {
 							?>
 							<td align="center" width=<?php echo $pourc_icone; ?>><a href="etat_major.php" target='_blank'><img width=117 height=89 border=0 src="../images/<?php echo $image_em; ?>" alt="etat major"></a></td>
@@ -2440,6 +2446,9 @@ if($dispo || $admin){
 								?>
 							</td>
 							<td align="center" width=<?php echo $pourc_icone; ?>><a href="classement.php" target='_blank'><img width=83 height=16 border=0 src="../images/classement_titrev2.png"></a></td>
+							<?php
+							if ($type_perso != 6) {
+							?>
 							<td align="center" width=<?php echo $pourc_icone; ?>>
 								<a href="compagnie.php" target='_blank'><img width=83 height=16 border=0 src="../images/compagnie_titrev2.png"></a>
 								<?php
@@ -2457,6 +2466,7 @@ if($dispo || $admin){
 								?>
 							</td>
 							<?php
+							}
 							if ($nb_em) {
 							?>
 							<td align="center" width=<?php echo $pourc_icone; ?>>
