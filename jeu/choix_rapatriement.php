bb6ae3c2620b8de376fb3260fdb3a46d1231a11c
loka <romain.perruchon@gmail.com>
2020-09-12 21:30:34 +0200

    Correction redirection

--- jeu/choix_rapatriement.php
+++ jeu/choix_rapatriement.php
@@ -406,7 +406,7 @@ if(isset($_SESSION["id_perso"])){
 		$_SESSION = array(); // On écrase le tableau de session
 		session_destroy(); // On détruit la session
 		
-		header("Location:../index.php");
+		header("Location:../index2.php");
 	}
 }
 else{
