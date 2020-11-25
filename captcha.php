c6f975a76a9d5638000f3660afadde6520d11d9d
loka <romain.perruchon@gmail.com>
2020-11-25 19:29:59 +0100

    maj captcha + refresh captcha page index au clic

--- captcha.php
+++ captcha.php
@@ -14,25 +14,36 @@ $matrix_blur = array(
 		array(1,2,1));
 
 $mot				= motListe();
-$largeur 			= strlen($mot) * 12;
+$tab_mot			= str_split($mot);
+$largeur 			= 140;
 $hauteur 			= 24;
 $milieuHauteur 		= ($hauteur / 2) - 8;
 $_SESSION["code"]	= $mot;
 
 $image 		= imagecreatetruecolor($largeur, $hauteur);
 $background = array(
-		imagecolorallocate($image, 0x99, 0x00, 0x66),
-		imagecolorallocate($image, 0xCC, 0x00, 0x00),
-		imagecolorallocate($image, 0x44, 0x44, 0xCC),
-		imagecolorallocate($image, 0xBB, 0x88, 0x77));
+		imagecolorallocate($image, 0xD7, 0x19, 0x1C),
+		imagecolorallocate($image, 0xFD, 0xAE, 0x61),
+		imagecolorallocate($image, 0xFF, 0xFF, 0xBF),
+		imagecolorallocate($image, 0x2C, 0x7B, 0xB6),
+		imagecolorallocate($image, 0xAB, 0xD9, 0xE9));
 $blanc 		= imagecolorallocate($image, 255, 255, 255); 
 $noir 		= imagecolorallocate($image, 0, 0, 0);
 $red		= imagecolorallocate($image, 245, 73, 73);
 
+$position_debut_mot = mt_rand(4,15);
+
 imagefill($image, 0, 0, $background[array_rand($background)]);
-imagestring($image, 6, 6 , $milieuHauteur, $mot, $noir);
+
+for ($i = 0; $i < strlen($mot); $i++) {
+	$taille_police = mt_rand(3,5);
+	$variation_hauteur = mt_rand(-4,4);
+	imagestring($image, $taille_police, $position_debut_mot + 12*$i , $milieuHauteur + $variation_hauteur, $tab_mot[$i], $noir);
+}
+
+//imagestring($image, 6, $position_debut_mot , $milieuHauteur, $mot, $noir);
 imagerectangle($image, 1, 1, $largeur - 1, $hauteur - 1, $noir);
-imageconvolution($image, $matrix_blur,16,0);
+imageconvolution($image, $matrix_blur, 16, 0);
 
 $ecart_line = mt_rand(4,7);
 $fin_line	= mt_rand(-10,+10);
@@ -48,5 +59,40 @@ header('Content-type: image/png');
 imagepng($image);
 imagedestroy($image);
 
+/*
+session_start();
+
+header('Content-Type: image/png');
+$largeur 	= 80;
+$hauteur	= 25;
+$lignes		= 10;
+
+$caracteres="ABCDEF123456789";
+
+$image = imagecreatetruecolor($largeur, $hauteur);
+
+imagefilledrectangle($image, 0, 0, $largeur, $hauteur, imagecolorallocate($image, 255, 255, 255));
+
+function hexargb($hex) {
+    return array("r"=>hexdec(substr($hex,0,2)),"g"=>hexdec(substr($hex,2,2)),"b"=>hexdec(substr($hex,4,2)));
+}
+
+for($i=0;$i<=$lignes;$i++){
+    $rgb=hexargb(substr(str_shuffle("ABCDEF0123456789"),0,6));
+    imageline($image,rand(1,$largeur-25),rand(1,$hauteur),rand(1,$largeur+25),rand(1,$hauteur),imagecolorallocate($image, $rgb['r'], $rgb['g'], $rgb['b']));
+}
+
+$code1=substr(str_shuffle($caracteres),0,4);
+$_SESSION['code']=$code1;
+$code="";
+
+for($i=0;$i<=strlen($code1);$i++){
+    $code .=substr($code1,$i,1)." ";
+}
+
+imagestring($image, 5, 10, 5, $code, imagecolorallocate($image, 0, 0, 0));
+imagepng($image);
+imagedestroy($image);
+*/
 
 ?>
--- index.php
+++ index.php
@@ -91,7 +91,7 @@ $count_online = $res->fetch_array();
 								<input name="pseudo" type="text" id="pseudo" placeholder="pseudo" maxlength="30">
 								<input name="password" type="password" id="password" placeholder="password" maxlength="20"><br />
 								<input name="captcha" type="text" placeholder="Entrer le texte dans l'image">
-								<img src="captcha.php" style="vertical-align: middle;"/><br />
+								<a href='#' id='reload'><img src="captcha.php" id='captcha' style="vertical-align: middle;"/></a><br />
 								<input type="submit" name="Submit" value="Se connecter">
 							</form>
 							
@@ -201,5 +201,11 @@ $count_online = $res->fetch_array();
 		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+		
+		<script>
+		$('#reload').click(function(){
+			$("#captcha").attr("src", "captcha.php?"+(new Date()).getTime());
+		})
+		</script>
 	</body>
 </html>
