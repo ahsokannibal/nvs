1e5a7e27419c7b2a5bcb5bdba13334ddf11b578b
loka <romain.perruchon@gmail.com>
2020-12-06 21:52:23 +0100

    - Console animation : Possibilité d'envoyer un message à tous les persos de son camp - Console animation : Possibilité d'envoyer un message à tous les chefs de compagnie / section de son camp - Console animation : Possibilité d'envoyer un message à tous les persos du jeu

--- jeu/anim_message.php
+++ jeu/anim_message.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 require_once("../fonctions.php");
+require_once("f_carte.php");
 
 $mysqli = db_connexion();
 
@@ -17,23 +18,35 @@ if($dispo || $admin){
 		//recuperation des variables de sessions
 		$id = $_SESSION["id_perso"];
 		
-		// Le perso est-il membre de l'etat major
-		$sql = "SELECT camp_em FROM perso_in_em WHERE id_perso='$id'";
-		$res = $mysqli->query($sql);
-		$t = $res->fetch_assoc();
-		$verif = $res->num_rows;
+		$mess			= "";
+		$mess_erreur	= "";
 		
-		if ($verif) {
+		if (anim_perso($mysqli, $id)) {
 			
-			$msg_erreur = "";
-			$msg = "";
-		
-			$camp_em = $t['camp_em'];
+			// Récupération du camp de l'animateur 
+			$sql = "SELECT clan FROM perso WHERE id_perso='$id'";
+			$res = $mysqli->query($sql);
+			$t = $res->fetch_assoc();
 			
-			if ($camp_em == 1) {
-				$image_em = "em_nord.png";
-			} else {
-				$image_em = "em_sud.png";
+			$camp = $t['clan'];
+			
+			if (isset($_GET['cible'])) {
+				
+				$cible = $_GET['cible'];
+				
+				if ($cible == "camp") {
+					$titre = "Message à son camp";
+				}
+				else if ($cible == "compagnie") {
+					$titre = "Message à destination des chefs de compagnies / sections";
+				}
+				else if ($cible == "all") {
+					$titre = "Message à tous les persos";
+				}
+			}
+			else {
+				$cible = "camp";
+				$titre = "Message à son camp";
 			}
 			
 			// Envoi message
@@ -45,7 +58,7 @@ if($dispo || $admin){
 					
 					if ($cible == "camp") {
 						// Récupération de tous les persos de mon camp
-						$sql_m = "SELECT id_perso FROM perso WHERE clan='$camp_em'";
+						$sql_m = "SELECT id_perso FROM perso WHERE clan='$camp'";
 						$res_m = $mysqli->query($sql_m);						
 					}
 					else if ($cible == "compagnie") {
@@ -53,13 +66,12 @@ if($dispo || $admin){
 						$sql_m = "SELECT perso.id_perso FROM perso, perso_in_compagnie 
 								WHERE perso.id_perso = perso_in_compagnie.id_perso
 								AND perso_in_compagnie.poste_compagnie='1'
-								AND clan='$camp_em'";
+								AND clan='$camp'";
 						$res_m = $mysqli->query($sql_m);
 					}
-					else if ($cible == "em") {
-						// Récupération de tous les membres de l'EM de mon camp
-						$sql_m = "SELECT id_perso FROM perso_in_em 
-								WHERE camp_em='$camp_em'";
+					else if ($cible == "all") {
+						// Récupération de tous les persos
+						$sql_m = "SELECT id_perso FROM perso";
 						$res_m = $mysqli->query($sql_m);
 					}
 					else {
@@ -76,7 +88,7 @@ if($dispo || $admin){
 						$expediteur = $t_p['nom_perso'];
 						$message = addslashes($_POST["message"]);
 						if(trim($_POST["objet"]) == ""){
-							$txt_objet = "[Important] Message de l'Etat Major";
+							$txt_objet = "[Important] Message de l'Animation";
 						}
 						else {
 							$txt_objet = $_POST["objet"];
@@ -104,23 +116,23 @@ if($dispo || $admin){
 							$mysqli->query($sql);
 						}
 						
-						$msg = "Messages envoyés";
+						$mess = "Messages envoyés";
 						
 					}
 					else {
-						$msg_erreur = "Cible incorrecte";
+						$mess_erreur = "Cible incorrecte";
 					}
 				}
 				else {
-					$msg_erreur = "Cible non défini";
+					$mess_erreur = "Cible non défini";
 				}
 			}
-?>
-		
+			
+			?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 	<head>
-		<title>Nord VS Sud - Etat Major</title>
+		<title>Nord VS Sud - Animation</title>
 		
 		<!-- Required meta tags -->
 		<meta charset="utf-8">
@@ -130,83 +142,40 @@ if($dispo || $admin){
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 	</head>
 	<body>
-		<div class="container">
-			
-			<nav class="navbar navbar-expand-lg navbar-light bg-light">
-				<a class="navbar-brand" href="#"><img src='../images/<?php echo $image_em; ?>' width="80" height="60" alt=""></a>
-				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-					<span class="navbar-toggler-icon"></span>
-				</button>
-				<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-					<div class="navbar-nav">
-						<ul class="navbar-nav">
-							<li class="nav-item">
-								<a class="nav-link" href="etat_major.php">Validation compagnies</a>
-							</li>
-						</ul>
-						<ul class="navbar-nav">
-							<li class="nav-item dropdown">
-								<a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLinkCarte" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-									Carte suivante
-								</a>
-								<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLinkCarte">
-									<a class="dropdown-item" href="em_cartes_suivante.php">Voir les carte suivante</a>
-									<a class="dropdown-item" href="em_definir_infra.php">Positionner les infrastructures</a>
-								</div>
-							</li>
-						</ul>
-						<ul class="navbar-nav">
-							<li class="nav-item dropdown active">
-								<a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-									Messages
-								</a>
-								<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-									<a class="dropdown-item" href="em_message.php?cible=camp">Message à son camp</a>
-									<a class="dropdown-item" href="em_message.php?cible=compagnie">Message aux chefs de compagnie / section</a>
-									<a class="dropdown-item" href="em_message.php?cible=em">Messages aux autres membres de l'EM</a>
-								</div>
-							</li>
-						</ul>
-					</div>
-				</div>
-			</nav>
+		<div class="container-fluid">
 		
-			<p align="center"><input type="button" value="Fermer cette fenêtre" onclick="window.close()"></p>
-			
-			<?php
-			if (isset($_GET['cible'])) {
-				$cible = $_GET['cible'];
-				
-				if ($cible == "camp") {
-					$titre = "Message pour tous les persos de mon camp";
-				}
-				else if ($cible == "compagnie") {
-					$titre = "Message à destination des chefs de compagnies / sections";
-				}
-				else if ($cible == "em") {
-					$titre = "Message à destination des membres de l'EM";
-				}
-				else {
-					$titre = "";
-				}
-			}
-			?>
-			<div class="row justify-content-center">
+			<div class="row">
 				<div class="col-12">
-				<?php 
-				if (isset($msg_erreur) && trim($msg_erreur) != "") {
-					echo "<font color='red'>".$msg_erreur."</font>";
-				}
-				if (isset($msg) && trim($msg) != "") {
-					echo "<font color='blue'>".$msg."</font>";
-				}
-				?>
+					<div align="center">
+						<h2>Animation - <?php echo $titre; ?></h2>
+					</div>
 				</div>
 			</div>
 			
-			<h1><?php echo $titre; ?></h1>
+			<p align="center"><a class="btn btn-primary" href="animation.php">Retour page principale d'animation</a></p>
 			
-			<div class="row justify-content-center">
+			<div class="row">
+				<div class="col-12">
+					<div align="center">
+						<a class="btn btn-warning" href="anim_message.php?cible=camp">Message à son camp</a>
+						<a class="btn btn-warning" href="anim_message.php?cible=compagnie">Message aux chefs de compagnie</a>
+						<a class="btn btn-warning" href="anim_message.php?cible=all">Message à tous</a>
+					</div>
+				</div>
+			</div>
+			
+			<div class="row">
+				<div class="col-12">
+					<div align="center">
+						<?php
+						echo "<font color='blue'>".$mess."</font><br />";
+						echo "<font color='red'><b>".$mess_erreur."</b></font><br />";
+						?>
+					</div>
+				</div>
+			</div>
+			
+			<div class="row">
 				<div class="col-12">
 					<form method="post" action="">
 						<table border=1 align="center" cellpadding=2 cellspacing=1 width=100%>
@@ -227,7 +196,7 @@ if($dispo || $admin){
 								<td colspan=3 align="center"><TEXTAREA class="form-control" id="textareaMessageImput" name="message" rows="15" cols="50" ></TEXTAREA></td>
 							</tr>
 						</table>
-						<div align="center"><INPUT TYPE="SUBMIT" name="envoyer" VALUE="envoyer"></div>
+						<div align="center"><input type="submit" name="envoyer" value="envoyer" class='btn btn-success'></div>
 					</form>
 				</div>
 			</div>
@@ -241,13 +210,12 @@ if($dispo || $admin){
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 	</body>
 </html>
-
-
-<?php
+			
+		<?php
 		}
 		else {
-			// Un joueur essaye d'acceder à la page sans être de l'état major
-			$text_triche = "Tentative accés page etat major sans y avoir les droits";
+			// Un joueur essaye d'acceder à la page sans être animateur
+			$text_triche = "Tentative accés page animation sans y avoir les droits";
 			
 			$sql = "INSERT INTO tentative_triche (id_perso, texte_tentative) VALUES ('$id', '$text_triche')";
 			$mysqli->query($sql);
@@ -266,4 +234,3 @@ else {
 	
 	header("Location:../index2.php");
 }
-?>
