10647543991b58a263c353c8aca288fa1e372081
loka <romain.perruchon@gmail.com>
2020-12-10 18:46:55 +0100

    limite 20xp par tour

--- bdd/nvs_tables.sql
+++ bdd/nvs_tables.sql
@@ -928,7 +928,8 @@ CREATE TABLE `perso` (
   `chef` tinyint(1) NOT NULL DEFAULT '0',
   `bataillon` varchar(250) NOT NULL DEFAULT '',
   `convalescence` INT NOT NULL DEFAULT '0',
-  `genie` INT NOT NULL DEFAULT '0'
+  `genie` INT NOT NULL DEFAULT '0',
+  `gain_xp_tour` INT NOT NULL DEFAULT '0'
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
