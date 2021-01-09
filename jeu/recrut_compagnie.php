2bec2ac0a219ea44eb2afa3b3d1278f10ae9bb68
loka <romain.perruchon@gmail.com>
2021-01-09 17:35:06 +0100

    Correction soucis requete maj thune banque compagnie lors départ perso

--- jeu/recrut_compagnie.php
+++ jeu/recrut_compagnie.php
@@ -205,7 +205,7 @@ if(isset($_SESSION["id_perso"])){
 						
 							if ($thune_en_banque > 0) {
 								$sql = "UPDATE banque_as_compagnie SET montant = montant - $thune_en_banque 
-										WHERE id_compagnie='$id_compagnie')";
+										WHERE id_compagnie='$id_compagnie'";
 								$mysqli->query($sql);
 								
 								$sql = "SELECT montant FROM banque_as_compagnie WHERE id_compagnie='$id_compagnie'";
