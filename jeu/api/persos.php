eccd2adb00e8dbe7f0730f0d8ddbac8ddbd406bf
loka <romain.perruchon@gmail.com>
2021-01-14 18:50:31 +0100

    api persos

--- jeu/api/persos.php
+++ jeu/api/persos.php
@@ -6,17 +6,18 @@ $mysqli = db_connexion();
 if(isset($_GET['term'])){
 	$search = $mysqli->real_escape_string($_GET['term']);
 
-	$query = "SELECT id_perso, nom_perso FROM perso WHERE nom_perso like'%".$search."%'";
+	$query = "SELECT id_perso, nom_perso, clan FROM perso WHERE nom_perso like'%".$search."%'";
 	$result = $mysqli->query($query);
 
 	$response = array();
 
 	while($t = $result->fetch_assoc()){
 		
-		$id_perso = $t['id_perso'];
-		$nom_perso = $t['nom_perso'];
+		$id_perso 	= $t['id_perso'];
+		$nom_perso 	= $t['nom_perso'];
+		$camp_perso	= $t['clan'];
 		
-		$response[] = array("value"=>$id_perso,"label"=>$nom_perso);
+		$response[] = array("value"=>$id_perso,"label"=>$nom_perso,"camp"=>$camp_perso);
 	}
 	
 	echo json_encode($response);
