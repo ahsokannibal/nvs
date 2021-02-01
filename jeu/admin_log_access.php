af9dd8172b85e591474256fddcff09ea71786628
loka <romain.perruchon@gmail.com>
2021-02-01 23:45:06 +0100

    Possibilité de consulter les stats par jour, par mois et par an Ajout d'une librairie permettant de faire des graphiques Possibilité de consulter le graphique des logs par heure et par page pour une journée choisie

--- jeu/admin_log_access.php
+++ jeu/admin_log_access.php
@@ -94,23 +94,50 @@ if(isset($_SESSION["id_perso"])){
 				<div class="col-12">
 					<div align="center">	
 						<?php
+						if (!isset($_GET['detail_complet'])) {
+							echo "<a href='admin_log_access.php?id_perso=".$id_perso_select."&detail_complet=ok' class='btn btn-secondary'>Détail Complet des logs</a> ";
+						}
+						
 						if (isset($_GET['stat_jour'])) {
-							echo "<a href='admin_log_access.php?id_perso=".$id_perso_select."' class='btn btn-warning'>Détail des logs</a>";
+							echo "<a href='admin_log_access.php?id_perso=".$id_perso_select."&stat_mois=ok' class='btn btn-warning'>Statistiques par mois</a> ";
+							echo "<a href='admin_log_access.php?id_perso=".$id_perso_select."&stat_annee=ok' class='btn btn-warning'>Statistiques par année</a>";
+						}
+						elseif (isset($_GET['stat_mois'])) {
+							echo "<a href='admin_log_access.php?id_perso=".$id_perso_select."&stat_jour=ok' class='btn btn-warning'>Statistiques par jour</a> ";
+							echo "<a href='admin_log_access.php?id_perso=".$id_perso_select."&stat_annee=ok' class='btn btn-warning'>Statistiques par année</a>";
+						}
+						elseif (isset($_GET['stat_annee'])) {
+							echo "<a href='admin_log_access.php?id_perso=".$id_perso_select."&stat_jour=ok' class='btn btn-warning'>Statistiques par jour</a> ";
+							echo "<a href='admin_log_access.php?id_perso=".$id_perso_select."&stat_mois=ok' class='btn btn-warning'>Statistiques par mois</a>";
 						}
 						else {
-							echo "<a href='admin_log_access.php?id_perso=".$id_perso_select."&stat_jour=ok' class='btn btn-warning'>Statistiques par jour</a>";
+							echo "<a href='admin_log_access.php?id_perso=".$id_perso_select."&stat_jour=ok' class='btn btn-warning'>Statistiques par jour</a> ";
+							echo "<a href='admin_log_access.php?id_perso=".$id_perso_select."&stat_mois=ok' class='btn btn-warning'>Statistiques par mois</a> ";
+							echo "<a href='admin_log_access.php?id_perso=".$id_perso_select."&stat_annee=ok' class='btn btn-warning'>Statistiques par année</a>";
 						}
 						echo "<br /><br />";
 						?>
 						<div id="table_logs_acces" class="table-responsive">
 							<?php
 							if (isset($_GET['stat_jour'])) {
-								$sql = "SELECT DAY(date_acces) as jour, COUNT(*) as nb_logs
+								$sql = "SELECT DAY(date_acces) as jour, MONTH(date_acces) as mois, YEAR(date_acces) as annee, COUNT(*) as nb_logs
 									FROM acces_log
-									WHERE  id_perso='$id_perso_select'
+									WHERE id_perso='$id_perso_select'
 									GROUP BY YEAR(date_acces), MONTH(date_acces), DAY(date_acces)";
 							}
-							else {
+							elseif (isset($_GET['stat_mois'])) {
+								$sql = "SELECT MONTH(date_acces) as mois, YEAR(date_acces) as annee, COUNT(*) as nb_logs
+									FROM acces_log
+									WHERE id_perso='$id_perso_select'
+									GROUP BY YEAR(date_acces), MONTH(date_acces)";
+							}
+							elseif (isset($_GET['stat_annee'])) {
+								$sql = "SELECT YEAR(date_acces) as annee, COUNT(*) as nb_logs
+									FROM acces_log
+									WHERE id_perso='$id_perso_select'
+									GROUP BY YEAR(date_acces)";
+							}
+							elseif (isset($_GET['detail_complet'])) {
 								$sql = "SELECT * FROM acces_log WHERE id_perso='$id_perso_select' ORDER BY id_acces DESC";
 							}
 							$res = $mysqli->query($sql);
@@ -120,9 +147,20 @@ if(isset($_SESSION["id_perso"])){
 							echo "		<tr>";
 							if (isset($_GET['stat_jour'])) {
 								echo "			<th style='text-align:center'>Jour</th>";
-								echo "			<th style='text-align:center'>nb logs</th>";
+								echo "			<th style='text-align:center'>Nb logs</th>";
+								echo "			<th style='text-align:center'>Action</th>";
 							}
-							else {
+							elseif (isset($_GET['stat_mois'])) {
+								echo "			<th style='text-align:center'>Jour</th>";
+								echo "			<th style='text-align:center'>Nb logs</th>";
+								echo "			<th style='text-align:center'>Action</th>";
+							}
+							elseif (isset($_GET['stat_annee'])) {
+								echo "			<th style='text-align:center'>Jour</th>";
+								echo "			<th style='text-align:center'>Nb logs</th>";
+								echo "			<th style='text-align:center'>Action</th>";
+							}
+							elseif (isset($_GET['detail_complet'])) {
 								echo "			<th style='text-align:center'>Date accès</th>";
 								echo "			<th style='text-align:center'>Page</th>";
 							}
@@ -134,14 +172,46 @@ if(isset($_SESSION["id_perso"])){
 								
 								if (isset($_GET['stat_jour'])) {
 									$jour		= $t['jour'];
+									$mois		= $t['mois'];
+									$annee		= $t['annee'];
 									$nb_logs	= $t['nb_logs'];
 									
 									echo "		<tr>";
-									echo "			<td align='center'>".$jour."</td>";
+									echo "			<td align='center'>".sprintf('%02d', $jour)."/".sprintf('%02d', $mois)."/".$annee."</td>";
 									echo "			<td align='center'>".$nb_logs."</td>";
+									echo "			<td align='center'>";
+									echo "				<a href='admin_log_access.php?id_perso=".$id_perso_select."&stat_jour=ok&jour=".$jour."&mois=".$mois."&annee=".$annee."' class='btn btn-primary'>Détail logs</a>";
+									echo "				<a href='admin_log_access.php?id_perso=".$id_perso_select."&graph_jour=ok&jour=".$jour."&mois=".$mois."&annee=".$annee."' class='btn btn-primary'>Graphique</a>";
+									echo "				<a href='anim_event_perso.php?id_perso=".$id_perso_select."&jour=".$jour."&mois=".$mois."&annee=".$annee."' target='_blank' class='btn btn-secondary'>Événements détaillés</a>";
+									echo "			</td>";
 									echo "		</tr>";
 								}
-								else {
+								elseif (isset($_GET['stat_mois'])) {
+									$mois		= $t['mois'];
+									$annee		= $t['annee'];
+									$nb_logs	= $t['nb_logs'];
+									
+									echo "		<tr>";
+									echo "			<td align='center'>".sprintf('%02d', $mois)." / ".$annee."</td>";
+									echo "			<td align='center'>".$nb_logs."</td>";
+									echo "			<td align='center'>";
+									echo "				<a href='admin_log_access.php?id_perso=".$id_perso_select."&stat_jour=ok&mois=".$mois."&annee=".$annee."' class='btn btn-primary'>Détail</a>";
+									echo "			</td>";
+									echo "		</tr>";
+								}
+								elseif (isset($_GET['stat_annee'])) {
+									$annee		= $t['annee'];
+									$nb_logs	= $t['nb_logs'];
+									
+									echo "		<tr>";
+									echo "			<td align='center'>".$annee."</td>";
+									echo "			<td align='center'>".$nb_logs."</td>";
+									echo "			<td align='center'>";
+									echo "				<a href='admin_log_access.php?id_perso=".$id_perso_select."&stat_jour=ok&annee=".$annee."' class='btn btn-primary'>Détail</a>";
+									echo "			</td>";
+									echo "		</tr>";
+								}
+								elseif (isset($_GET['detail_complet'])) {
 									$date_acces	= $t['date_acces'];
 									$page_acces	= $t['page'];
 									
@@ -166,11 +236,179 @@ if(isset($_SESSION["id_perso"])){
 		
 		</div>
 		
+		<div class="row my-2">
+			<div class="col-md-2"></div>
+			<div class="col-md-8">
+				<div class="card">
+					<?php
+					if (isset($_GET['graph_jour']) && isset($_GET['jour']) && isset($_GET['mois']) && isset($_GET['annee'])) {
+						
+						$jour 	= $_GET['jour'];
+						$mois	= $_GET['mois'];
+						$annee	= $_GET['annee'];
+						
+						echo "<center>Graphique par heure du jour ".sprintf('%02d', $jour)."/".sprintf('%02d', $mois)."/".$annee."</center>";
+						
+						$data_log_jouer 	= array();
+						$data_log_evenement = array();
+						
+						$heure_jouer_tmp 		= 0;
+						$heure_evenement_tmp	= 0;
+						
+						$sql = "SELECT COUNT(*) as nb_logs_jouer, HOUR(date_acces) as heure
+									FROM acces_log
+									WHERE id_perso='$id_perso_select'
+									AND page LIKE 'jouer.php%'
+									AND YEAR(date_acces) = '$annee'
+									AND MONTH(date_acces) = '$mois'
+									AND DAY(date_acces) = '$jour'
+									GROUP BY HOUR(date_acces)";
+						$res = $mysqli->query($sql);
+						
+						while ($t = $res->fetch_assoc()) {
+							$nb_logs_jouer 	= $t['nb_logs_jouer'];
+							$heure_jouer	= $t['heure'];
+							
+							if ($heure_jouer_tmp == 0) {
+								for ($i = 0; $i < $heure_jouer; $i++) {
+									array_push($data_log_jouer, 0);
+								}
+								
+								$heure_jouer_tmp = $heure_jouer;
+							}
+							else {
+								for ($i = $heure_jouer_tmp + 1; $i < $heure_jouer; $i++) {
+									array_push($data_log_jouer, 0);
+								}
+								
+								$heure_jouer_tmp = $heure_jouer;
+							}
+							
+							array_push($data_log_jouer, $nb_logs_jouer);
+						}
+						
+						$sql = "SELECT COUNT(*) as nb_logs_evenement, HOUR(date_acces) as heure
+									FROM acces_log
+									WHERE id_perso='$id_perso_select'
+									AND page LIKE 'evenement.php%'
+									AND YEAR(date_acces) = '$annee'
+									AND MONTH(date_acces) = '$mois'
+									AND DAY(date_acces) = '$jour'
+									GROUP BY HOUR(date_acces)";
+						$res = $mysqli->query($sql);
+						
+						while ($t = $res->fetch_assoc()) {
+							$nb_logs_evenement 	= $t['nb_logs_evenement'];
+							$heure_evenement	= $t['heure'];
+							
+							if ($heure_evenement_tmp == 0) {
+								for ($i = 0; $i < $heure_evenement; $i++) {
+									array_push($data_log_evenement, 0);
+								}
+								
+								$heure_evenement_tmp = $heure_evenement;
+							}
+							else {
+								for ($i = $heure_evenement_tmp + 1; $i < $heure_evenement; $i++) {
+									array_push($data_log_evenement, 0);
+								}
+								
+								$heure_evenement_tmp = $heure_evenement;
+							}
+							
+							array_push($data_log_evenement, $nb_logs_evenement);
+						}
+						
+						echo "<canvas id='chBar'></canvas>";
+					}
+					?>
+				</div>
+			</div>
+			<div class="col-md-2"></div>
+		</div>
+		
 		<!-- Optional JavaScript -->
 		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
 		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.bundle.min.js "></script>
+		
+		<script>
+		// chart colors
+		var colors = ['#007bff','#28a745','#333333','#c3e6cb','#dc3545','#6c757d'];		
+		
+		var chBar = document.getElementById("chBar");
+		if (chBar) {
+			new Chart(chBar, {
+				type: 'bar',
+				data: {
+					labels: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23"],
+					datasets: [{
+						label: 'jouer.php',
+						data: <?php echo json_encode($data_log_jouer); ?>,
+						backgroundColor: colors[0]
+					},
+					{
+						label: 'evenement.php',
+						data: <?php echo json_encode($data_log_evenement); ?>,
+						backgroundColor: colors[1]
+					}]
+				},
+				options: {
+					legend: {
+						display: true
+					},
+					scales: {
+						xAxes: [{
+							barPercentage: 0.4,
+							categoryPercentage: 0.5
+						}]
+					}
+				}
+			});
+		}
+		
+		/*
+		var ctx = document.getElementById('myChart').getContext('2d');
+		var myChart = new Chart(ctx, {
+			type: 'bar',
+			data: {
+				labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+				datasets: [{
+					label: 'nombre de logs',
+					data: [12, 19, 3, 5, 2, 3],
+					backgroundColor: [
+						'rgba(255, 99, 132, 0.2)',
+						'rgba(54, 162, 235, 0.2)',
+						'rgba(255, 206, 86, 0.2)',
+						'rgba(75, 192, 192, 0.2)',
+						'rgba(153, 102, 255, 0.2)',
+						'rgba(255, 159, 64, 0.2)'
+					],
+					borderColor: [
+						'rgba(255, 99, 132, 1)',
+						'rgba(54, 162, 235, 1)',
+						'rgba(255, 206, 86, 1)',
+						'rgba(75, 192, 192, 1)',
+						'rgba(153, 102, 255, 1)',
+						'rgba(255, 159, 64, 1)'
+					],
+					borderWidth: 1
+				}]
+			},
+			options: {
+				scales: {
+					yAxes: [{
+						ticks: {
+							beginAtZero: true
+						}
+					}]
+				}
+			}
+		});
+		*/
+		</script>
 	</body>
 </html>
 <?php
