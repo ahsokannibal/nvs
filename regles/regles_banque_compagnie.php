c937769c36e93866a985ea70878fdd82d2b109f4
loka <romain.perruchon@gmail.com>
2021-03-01 17:44:51 +0100

    maj regle capture regles banque compagnie

---
+++ regles/regles_banque_compagnie.php
@@ -0,0 +1,58 @@
+<html>
+	<head>
+		<title>Nord VS Sud</title>
+		
+		<!-- Required meta tags -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	</head>
+
+	<body style="background-color:grey;">
+
+		<div class="container-fluid">
+			
+			<div class="row justify-content-center">
+				<div class="col-12">
+	
+					<div align="center"><h2>Règles banque compagnie</h2></div>
+					
+				</div>
+			</div>
+			
+			<div class="row">
+				<div class="col-12">
+					<h2>Dépôt de thunes</h2>
+					<p>Pour pouvoir déposer de la thune dans la banque de la compagnie, vous devez respecter plusieurs règles :</p>
+					<ul>
+						<li>Déposer au minimum 25 thunes (et donc en posséder au moins 25 sur vous)</li>
+						<li>Avoir un délai de 8h entre votre dernier retrait et votre dépôt</li>
+					</ul>
+					<h2>Retrait de thunes</h2>
+					<p>Pour pouvoir retirer de la thune de la banque de la compagnie, vous devez simplement posséder au minimum le montant que vous souhaitez retirer (il n'est pas possible de retirer un montant que vous ne possédez pas)</p>
+					<h2>Emprunt de thunes</h2>
+					<p>Lorsque vous avez besoin d'un certain montant de thunes et que vous ne possédez pas ce montant mais que la banque de la compagnie le possède, vous pouvez faire une demande d'emprunt à la banque.</p>
+					<p>Cette demande d'emprunt arrivera sur la console du trésorier et du chef de la compagnie qui valideront ou refuseront la demande. Nous vous conseillons donc, afin de mettre toutes les chances de votre côté, de négocier cet emprunt avec ces personnes avant de faire votre demande.</p>
+					<p>Si l'emprunt est refusé, vous recevrez un MP vous notifiant de ce refus.</p>
+					<p>Si l'emprunt est validé, vous obtiendrez la thune sur votre compte perso dans la banque de la compagnie, vous pourrez alors le retirer quand vous en aurez besoin.</p>
+					<p><b>Attention !</b> Un emprunt implique beaucoup de choses :</p>
+					<ul>
+						<li>Vous devez rembourser cet emprunt et donc tous vos futurs dépôts passerons automatiquement, et sans votre validation, au remboursement de cet emprunt</li>
+						<li>Vous ne pouvez pas quitter la compagnie tant que vous n'aurez pas réglé votre dette ! (seul le chef de compagnie peut décider de virer votre perso en faisant fit de la dette)</li>
+						<li>Si la dette de l'emprunt est sur un grouillot, il vous sera impossible de virer ce grouillot tant qu'il n'aura pas réglé sa dette</li>
+					</ul>
+					<h2>Virement de thunes à un autre perso de la compagnie</h2>
+					<p>Vous avez la possibilité d'effectuer des virements vers un autre perso de la compagnie. Il n'existe pas de dette inscrite au niveau de la banque sur ces virements, à vous donc de vous arranger entre vous et de faire attention à ne pas virer votre thune sur le compte de n'importe qui.</p>
+				</div>
+			</div>
+		</div>
+		
+		<!-- Optional JavaScript -->
+		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+	</body>
+</html>
