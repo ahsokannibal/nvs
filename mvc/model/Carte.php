77778c3561b50eba482b2def71cf089e2ad2229c
Ahsoka <ahsokannibal@protonmail.com>
2022-02-10 22:32:59 +0000

    fix

---
+++ mvc/model/Carte.php
@@ -0,0 +1,29 @@
+<?php
+require_once("Model.php");
+
+class Carte extends Model
+{
+	public $x_carte;
+	public $y_carte;
+	public $occupee_carte;
+	public $fond_carte;
+	public $idPerso_carte;
+	public $image_carte;
+	public $vue_nord;
+	public $vue_sud;
+	public $coordonnees;
+
+	public function __set($name, $value) {}
+	
+	public function __get($name){
+		return $name;
+	}
+	
+	public function recupereVoisins($id_cible, $x_cible, $y_cible){
+		$db = $this->dbConnectPDO();
+		$sql = "SELECT idPerso_carte FROM carte WHERE x_carte >= $x_cible - 1 AND x_carte <= $x_cible + 1 AND y_carte >= $y_cible - 1 AND y_carte <= $y_cible + 1 AND occupee_carte = '1' AND idPerso_carte != '$id_cible'";
+		$request = $db->prepare($sql);
+		$request->execute();
+		return $request;
+	}
+}
