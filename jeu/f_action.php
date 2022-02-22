61a378a3429f6462e6ba03a28b3a1ee8bcf42b8b
Ahsoka <ahsokannibal@protonmail.com>
2022-02-22 19:46:57 +0000

    limite distance entre 2 ponts a 10 pm

--- jeu/f_action.php
+++ jeu/f_action.php
@@ -484,8 +484,8 @@ function verif_contraintes_construction_bat($mysqli, $id_bat, $camp_perso, $x_ba
 			}
 		}
 		
-		// 30 PM entre chaque pont
-		$distance_min_pont = 30;
+		// 20 PM entre chaque pont
+		$distance_min_pont = 20;
 		
 		$sql = "SELECT save_info_carte FROM carte 
 					WHERE x_carte >= $x_bat - $distance_min_pont
