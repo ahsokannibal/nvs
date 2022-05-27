48ae640062d87b9dee5ce3d33b06518b399d69a5
Ahsoka <ahsokannibal@protonmail.com>
2022-05-27 09:46:55 +0000

    configuration du nom de bdd dans config

--- mvc/model/Model.php
+++ mvc/model/Model.php
@@ -7,7 +7,7 @@ abstract class Model
     protected function dbConnectPDO()
     {
 		try{
-			$db = new PDO('mysql:host=localhost;dbname=nvs;charset=utf8', BDD_LOGIN, BDD_PASSWORD);
+			$db = new PDO('mysql:host=localhost;dbname='.BDD_NAME.';charset=utf8', BDD_LOGIN, BDD_PASSWORD);
 			return $db;
 		}
 		catch(Exception $e) {
