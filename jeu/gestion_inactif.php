d43d8c4bcce7492301be2d80e9565033b57f7e08
Ahsoka <ahsokannibal@protonmail.com>
2022-05-28 22:09:25 +0000

    augmentation du delais avant suppression compte a 120/130 jours
    pour eviter les zombies qui se co sans jouer

--- jeu/gestion_inactif.php
+++ jeu/gestion_inactif.php
@@ -215,9 +215,9 @@ if (isset($_GET['clef']) && $_GET['clef'] == $clef_secrete) {
 	//***********************************************
 	// Envoi mail inactifs bientôt supprimés
 	//***********************************************
-	// On envoi un mail aux joueurs dont les persos sont gelés depuis 20 jours
+	// On envoi un mail aux joueurs dont les persos sont gelés depuis 120 jours
 	$sql = "SELECT DISTINCT(email_joueur) FROM perso, joueur WHERE perso.idJoueur_perso = joueur.id_joueur
-			AND date_gele < DATE_SUB(CURRENT_DATE, INTERVAL 20 DAY) AND est_gele='1' AND id_perso!='1' AND id_perso!='2'";
+			AND date_gele < DATE_SUB(CURRENT_DATE, INTERVAL 120 DAY) AND est_gele='1' AND id_perso!='1' AND id_perso!='2'";
 	$res = $mysqli->query($sql);
 
 	while ($t = $res->fetch_assoc()){
@@ -237,8 +237,8 @@ if (isset($_GET['clef']) && $_GET['clef'] == $clef_secrete) {
 	//***********************************************
 	// Traitement suppression des inactifs
 	//***********************************************
-	// On supprime les persos et le compte du joueur dont les persos sont gelés depuis 30 jours
-	$sql = "SELECT DISTINCT(joueur.id_joueur) FROM perso, joueur WHERE perso.idJoueur_perso = joueur.id_joueur AND date_gele < DATE_SUB(CURRENT_DATE, INTERVAL 30 DAY) AND est_gele='1' AND id_perso!='1' AND id_perso!='2'";
+	// On supprime les persos et le compte du joueur dont les persos sont gelés depuis 130 jours
+	$sql = "SELECT DISTINCT(joueur.id_joueur) FROM perso, joueur WHERE perso.idJoueur_perso = joueur.id_joueur AND date_gele < DATE_SUB(CURRENT_DATE, INTERVAL 130 DAY) AND est_gele='1' AND id_perso!='1' AND id_perso!='2'";
 	$res_sup = $mysqli->query($sql);
 
 	while ($t = $res_sup->fetch_assoc()){
