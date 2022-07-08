4beef5169f1e7593e1a78b213bddf68e460864bc
Ahsoka <ahsokannibal@protonmail.com>
2022-07-08 10:57:30 +0000

    changement degats gatling 2d90 vers 8d22

--- bdd/nvs_data.sql
+++ bdd/nvs_data.sql
@@ -87,7 +87,7 @@ INSERT INTO `arme` (`id_arme`, `nom_arme`, `porteeMin_arme`, `porteeMax_arme`, `
 (11, 'Bandages', 1, 1, 3, 50, 0, 0, 0, 0, 2, 2, 10, 35, '0', 0, '0.2', 0, 'Bandages permettant de récupérer des malus de defense', 0, 1, 'bandage.jpg'),
 (12, 'Griffes', 1, 1, 10, 0, 0, 0, 0, 0, 15, 15, 4, 90, '0', 0, '0.0', 0, '', 0, 1, 'griffe.png'),
 (13, 'Canon', 2, 6, 6, 0, 0, 0, 0, 0, 75, 75, 6, 65, '1', 0, '0.0', 0, 'Canon d\'artillerie, extrêmement dévastateur et efficace contre les bâtiments.', 0, 1, 'canon.jpg'),
-(14, 'Gatling', 1, 5, 3, 200, 0, 0, 0, 0, 2, 2, 90, 70, '1', 2, '0.0', 0, '', 0, 1, 'gatling.png'),
+(14, 'Gatling', 2, 5, 3, 200, 0, 0, 0, 0, 8, 8, 22, 70, '1', 2, '0.0', 0, '', 0, 1, 'gatling.png'),
 (15, 'Magnum', '1', '2', '3', '150', '0', '0', '0', '0', '16', '16', '8', '40', '0', '0', '2.0', '0', '', '0', '1', 'magnum.jpg'),
 (16, 'Couteau', '1', '1', '3', '10', '0', '0', '0', '0', '15', '15', '4', '35', '0', '0', '0.0', '0', '', '0', '1', 'couteau.png'),
 (17, 'Canon double', '1', '3', '5', '225', '0', '0', '0', '0', '20', '20', '8', '75', '0', '0', '3.0', '0', '', '0', '1', 'canon_double.png'),
