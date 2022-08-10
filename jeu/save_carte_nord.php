a5dbcb4bf4421d56d604d2ec0281db03381f37aa
Ahsoka <ahsokannibal@protonmail.com>
2022-08-10 19:25:59 +0000

    couleur barricade neutre

--- jeu/save_carte_nord.php
+++ jeu/save_carte_nord.php
@@ -191,6 +191,10 @@ while ($t = $res->fetch_assoc()){
 		$color = $couleur_bat_neutre;
 	}
 
+	// barricade
+	if ($id_bat == 1)
+		$color = $couleur_bat_neutre;
+
 	imagefilledrectangle ($perso_carte, (($x*3)-$taille_bat), (((600-($y*3)))-$taille_bat), (($x*3)+$taille_bat), (((600-($y*3)))+$taille_bat), $color);
 
 	// Met en évidence les points stratégiques
