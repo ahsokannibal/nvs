b50cc4b921021e368940c58b1cfa90db750f9dfe
FoIIon <46783727+FoIIon@users.noreply.github.com>
2022-08-17 09:09:44 +0200

    Nouvelle carte stratégique

--- jeu/statistiques/statistiques.php
+++ jeu/statistiques/statistiques.php
@@ -4,7 +4,7 @@ include("functions_statistiques.php");
 
 ?>
 
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<!DOCTYPE html">
 <html>
 	<head>
 		<title>Nord VS Sud - Statistiques</title>
