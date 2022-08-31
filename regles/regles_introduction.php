f92eddccb6b7ccb2ce57d18968b18efbbbd176fa
Julien MARTIN <ju.martin@outlook.com>
2022-08-31 10:19:59 +0200

    maj regles introduction

--- regles/regles_introduction.php
+++ regles/regles_introduction.php
@@ -76,21 +76,19 @@
 			<div class="row">
 				<div class="col-12">
 					<h2>Le Jeu</h2>
-Nord versus Sud est un jeu alliant stratégie et jeu de rôle dans l'univers du Far-West américain.<br />
-Vous y jouez un groupe de personnages et notamment un chef qui va effectuer une carrière militaire.<br />
-Les tours de jeu durent 46 heures. Il faut cependant impérativement se connecter après ces 46 heures pour activer le nouveau tour. Les tours ne commencent que quand vous vous connectez. Si vous oubliez votre tour ne commencera pas. 
+					<p>Nord versus Sud est un jeu alliant stratégie et jeu de rôle dans l'univers du Far-West américain. Vous y jouez un groupe de personnages et notamment un chef qui va effectuer une carrière militaire. Les tours de jeu durent 46 heures. Il faut cependant impérativement se connecter après ces 46 heures pour activer le nouveau tour. Les tours ne commencent que quand vous vous connectez. Si vous oubliez votre tour ne commencera pas.</p> 
+					
 					<h4>Votre camp</h4>
-Vous jouez un groupe de soldats Nordistes (en bleu sur la carte), Sudistes (en rouge sur la carte) composé d'un chef et de ses grouillots. 
+					<p>Vous jouez un groupe de soldats Nordistes (en bleu sur la carte), Sudistes (en rouge sur la carte) composé d'un chef et de ses grouillots.</p>
+					
 					<h4>Vos combattants</h4>
-Il est possible de jouer 5 sortes de combattants : des cavaliers, des infanteries, des artilleries, des soigneurs et des chiens.<br />
-Vous allez devoir vous constituer votre propre petite armée, que vous dirigerez. Vous pourrez intégrer une compagnie qui est un plus vaste regroupement de troupes.
+					<p>Il est possible de jouer **7** sortes de combattants : des cavaliers légers ou lourds, des infanteries,des gatlings, des artilleries, des soigneurs et des chiens. Vous allez devoir vous constituer votre propre petite armée, que vous dirigerez. Vous pourrez intégrer une compagnie qui est un plus vaste regroupement de troupes.</p>
 					
 					<h2>Histoire</h2>
-Personne ne sait exactement quand ni pour quels motifs la guerre fut déclenchée entre les territoires du Nord et ceux du Sud. Sans doute, une basse revendication territoriale en fut à l'origine. Toujours est il que depuis lors, la guerre fait rage et ne semble pas prendre fin. Les hommes tombent toujours plus nombreux, poussés au combat par leurs officiers supérieurs et galvanisés par la soif de gloire, de conquête et d'argent.<br />
-La légende veut qu'un jour la paix reviendra et que les hommes sauront s'unir. Mais le temps ne semble pas encore venu et chaque jour de nouvelles hordes s'affrontent, prêtes à tout pour vaincre leurs adversaires.<br />
+					<p>Personne ne sait exactement quand ni pour quels motifs la guerre fut déclenchée entre les territoires du Nord et ceux du Sud. Sans doute, une basse revendication territoriale en fut à l'origine. Toujours est il que depuis lors, la guerre fait rage et ne semble pas prendre fin. Les hommes tombent toujours plus nombreux, poussés au combat par leurs officiers supérieurs et galvanisés par la soif de gloire, de conquête et d'argent. La légende veut qu'un jour la paix reviendra et que les hommes sauront s'unir. Mais le temps ne semble pas encore venu et chaque jour de nouvelles hordes s'affrontent, prêtes à tout pour vaincre leurs adversaires.</p>
 
 					<h2>But du jeu</h2>
-Pour gagner la partie, il faut que l'un des camps remplisse les conditions de victoire établies sur chaque carte. Vous pouvez retrouver ces modalités de victoire <a href="regles_modalites_victoire.php">ici</a>
+					<p>Pour gagner la partie, il faut que l'un des camps remplisse les conditions de victoire établies pour chaque carte. Vous pouvez retrouver ces modalités de victoire <a href="regles_modalites_victoire.php">ici</a>.</p>
 				</div>
 			</div>
 		
