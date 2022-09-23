2485381c6f00631c8ea48f06de19eba7e6ab44eb
Ahsoka <ahsokannibal@protonmail.com>
2022-09-23 20:24:38 +0000

    correction fin de ligne

--- bdd/nvs_data.sql
+++ bdd/nvs_data.sql
@@ -171,8 +171,8 @@ INSERT INTO `objet_as_type_unite` (`id_objet`, `id_type_unite`) VALUES
 (7, 3),
 (7, 4),
 (7, 5),
-(7, 7);
-(8, 1);
+(7, 7),
+(8, 1),
 (9, 1);
 -- --------------------------------------------------------
 
@@ -434,8 +434,8 @@ INSERT INTO `objet` (`id_objet`, `nom_objet`, `portee_objet`, `bonusPerception_o
 (4, 'Trousse de soin', 0, 0, 60, 0, 0, 0, 0, 0, 0, 0, 50, '2.0', 'Une trousse de soin, permet de récupérer plus rapidement de ses blessures (Bonus Récup +60)', 'N'),
 (5, 'Bottes légères', 0, 0, 0, 0, 1, 0, 0, 0, -5, 0, 50, '2.5', 'Des bottes légères pour monter plus rapidement au front mais dont la protection proposée est plus faible (Bonus PM +1, Bonus Defense -5)', 'E'),
 (6, 'Longue vue', 0, 2, 0, 0, 0, 0, 0, -1, 0, 0, 200, '0.2', 'Une longue vue permettant d\augmenter la perception de celui qui l\utilise (Bonus Perception +2, Bonus PA -1)', 'E'),
-(7, 'Lunette de visée', 0, 0, 0, 0, 0, 0, 15, 0, 0, 0, 200, '0.2', 'Une lunette de tir pour augmenter la précision des tirs à courte et longue distance (Coût attaque à distance +2, Bonus Précision à distance +15)', 'E');
-(8, 'Etendard des armées du nord', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '9', "L'étendard de l'union", 'E');
+(7, 'Lunette de visée', 0, 0, 0, 0, 0, 0, 15, 0, 0, 0, 200, '0.2', 'Une lunette de tir pour augmenter la précision des tirs à courte et longue distance (Coût attaque à distance +2, Bonus Précision à distance +15)', 'E'),
+(8, 'Etendard des armées du nord', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '9', "L'étendard de l'union", 'E'),
 (9, 'Etendard des armées du sud', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '9', "L'étendard des armées du sud", 'E');
 
 --
