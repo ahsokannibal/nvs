5c2beb68c286522c2781da785665554b59563149
Ahsoka <ahsokannibal@protonmail.com>
2022-10-07 21:40:43 +0000

    brouillard de guerre a 2 jours

--- jeu/config.php
+++ jeu/config.php
@@ -7,8 +7,8 @@ define ("DUREE_ANTI_ZERK_S", 10*60*60);
 define ("BDD_NAME", "nvs");
 define ("BDD_LOGIN", "root");
 define ("BDD_PASSWORD", "yolo");
-// Brouillard de guerre de 5 jours
-define ("BROUILLARD_DE_GUERRE_S", 24*3600*5);
+// Brouillard de guerre de 2 jours
+define ("BROUILLARD_DE_GUERRE_S", 24*3600*2);
 // Difference nombre de joueurs max pour inscription
 define ("MAX_NB_JOUEUR_DIFF", 3);
 
