9b60860e15999762f28b916bc4ba07cae97b1fb3
Ahsoka <ahsokannibal@protonmail.com>
2022-11-01 21:23:48 +0000

    fix evement capture batiment
    deploiement:
    update evenement set phrase_evenement='a capture le batiment', nomCible_evenement='Plantation Calhoun' where IDCible_evenement=50002;
    update evenement set phrase_evenement='a capture le batiment', nomCible_evenement='Repere de la soif' where IDCible_evenement=50003;
    update evenement set phrase_evenement='a capture le batiment', nomCible_evenement='Refuge des planques' where IDCible_evenement=50004;
    update evenement set phrase_evenement='a capture le batiment', nomCible_evenement='Ranch du vieux Ned' where IDCible_evenement=50005;
    update evenement set phrase_evenement='a capture le batiment', nomCible_evenement='Ponton maudit' where IDCible_evenement=50006;

--- jeu/jouer.php
+++ jeu/jouer.php
@@ -1081,7 +1081,7 @@ if($dispo == '1' || $admin){
 																			}
 																				
 																			// mise a jour table evenement
-																			$sql = "INSERT INTO `evenement` (IDActeur_evenement, nomActeur_evenement, phrase_evenement, IDCible_evenement, nomCible_evenement, effet_evenement, date_evenement, special) VALUES ('$id_perso','<font color=$couleur_clan_p><b>$nom_perso</b></font>','a capturé le batiment $nom_bat','$id_inst_bat','','en $x_bat/$y_bat : Felicitation!',NOW(),'0')";
+																			$sql = "INSERT INTO `evenement` (IDActeur_evenement, nomActeur_evenement, phrase_evenement, IDCible_evenement, nomCible_evenement, effet_evenement, date_evenement, special) VALUES ('$id_perso','<font color=$couleur_clan_p><b>$nom_perso</b></font>','a capturé le batiment','$id_inst_bat','$nom_bat','en $x_bat/$y_bat : Felicitation!',NOW(),'0')";
 																			$mysqli->query($sql);
 																			
 																			// Gain points de victoire
