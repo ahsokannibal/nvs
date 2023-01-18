15d738853528f2e13bf0c045716bc846192950c8
Jacklegende <damien.wattiez1@gmail.com>
2023-01-18 09:27:11 +0100

    Revert "merge refacto sac"
    This reverts commit 2adebf02fc1b290d77d0a25c8e87445998125dff.

--- mvc/view/layouts/app.php
+++ mvc/view/layouts/app.php
@@ -16,17 +16,16 @@
 
         <!-- Styles -->
 			<!-- Bootstrap CSS -->
-			<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-			<link rel="stylesheet" href="../public/css/app.css">
+			<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+			<link rel="stylesheet" href="public/css/app.css">
 
         <!-- Scripts -->
 		<!-- Bunddle Popper.js & Bootstrap JS -->
 		<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous" defer></script>
-		<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
-		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js" integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous"></script>
-		<script type="text/javascript" src="../public/js/app.js" defer></script>
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous" defer></script>
+		<script type="text/javascript" src="public/js/app.js" defer></script>
     </head>
-    <body class='game'>
+    <body>
         <div class='app'>
             <?php //include('layouts.navigation') ?>
 
@@ -48,4 +47,3 @@
         </div>
     </body>
 </html>
-<?php unset($_SESSION["flash"]);?>
