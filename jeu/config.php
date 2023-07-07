2cddaf3e2fce1d4cfe8ce163028794e3d71ca113
MatthieuLiatard <31052514+MatthieuLiatard@users.noreply.github.com>
2023-07-07 00:56:07 -0400

    Revert "Added Pepite"
    This reverts commit 6010de56f1ba29685f5d1cff5f409b0da15bb762.

--- jeu/config.php
+++ jeu/config.php
@@ -6,9 +6,9 @@ define ("DUREE_ANTI_ZERK_S", 10*60*60);
 // Connexion a la bdd
 define ("BDD_NAME", "nvs");
 define ("BDD_LOGIN", "root");
-define ("BDD_PASSWORD", "");
-// Brouillard de guerre de 2 jours
-define ("BROUILLARD_DE_GUERRE_S", 24*3600*2);
+define ("BDD_PASSWORD", "yolo");
+// Brouillard de guerre de 1 jours
+define ("BROUILLARD_DE_GUERRE_S", 24*3600*1);
 // Difference nombre de joueurs max pour inscription
 define ("MAX_NB_JOUEUR_DIFF", 3);
 
