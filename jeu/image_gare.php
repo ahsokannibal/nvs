6550d70a9761f0aa9750f1c9c2f9ed7235219837
jacklegende <damien.wattiez1@gmail.com>
2023-07-24 21:47:02 +0200

    Reprisde correctif gare (#217)
    * correction de l'affichage des gare dans le batiment gare
    
    * reprise du code de génération plan gare

--- jeu/image_gare.php
+++ jeu/image_gare.php
@@ -2,47 +2,34 @@
 session_start();
 
 require_once("../fonctions.php");
+require_once("generer_plan_gare.php");
 
 $mysqli = db_connexion();
 
 if (isset($_SESSION["id_perso"])) {
-	
+
 	$id 		= $_SESSION["id_perso"];
-	
+
 	// Récupération camp
 	$sql = "SELECT clan FROM perso WHERE id_perso='$id'";
 	$res = $mysqli->query($sql);
 	$t = $res->fetch_assoc();
-	
-	$camp = $t['clan'];
-	
-	if ($camp == 1) {
-		$neg 			= -8;
-		$taille_camp 	= 4;
-		$camp_expected 	= "nord";
-	} else {
-		$neg 			= -7;
-		$taille_camp 	= 3;
-		$camp_expected 	= "sud";
-	}
 
-	if (isset($_GET['imagename'])) {
-		
-		$imagename 	= $_GET['imagename'];
-		$camp_img 	= substr($imagename, $neg, $taille_camp);
-		
-		if ($camp_img == $camp_expected) {
-		
-			$path = "carte";
+	$camp = (int)$t['clan'];
 
-			$fd = fopen ("$path/$imagename", "rb", 1);
-			$data = fread($fd, filesize("$path/$imagename"));
-			fclose ($fd);
-			print $data;
-			
-		} else {
-			header("location:../index.php");
-		}
+	if ($camp == 1 || $camp == 2) {
+		header("Content-type: image/png");//on va commencer par declarer que l'on veut creer une image
+		$imageOutput = new PlanGareImage($camp, $mysqli);
+		imagepng($imageOutput->GetImage());
+		//$imageOutput->Clear();
+
+		/* $path = "carte";
+
+		$fd = fopen ("$path/$imagename", "rb", 1);
+		$data = fread($fd, filesize("$path/$imagename"));
+		fclose ($fd);
+		print $data; */
+
 	} else {
 		header("location:../index.php");
 	}
