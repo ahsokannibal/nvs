ce0e1e91300e6e7f120cef4ec3f8eb92f0b9d739
Sivaintod <98672879+Sivaintod@users.noreply.github.com>
2023-08-23 23:51:52 +0200

    Optimisation de la DAO
    Optimisation des requêtes de la DAO
    + modifications mineures

--- mvc/model/Model.php
+++ mvc/model/Model.php
@@ -9,7 +9,11 @@ abstract class Model extends Db
 	protected $fillable = []; // seuls les champs de ce tableau seront autorisés dans une hydratation
 	protected $guarded; //  tous les champs de ce tableau seront enlevés d'une hydratation
 	
-	private $modelAttr = ['table','primaryKey','fillable','guarded','modelAttr','db'];
+	private $modelAttr = ['table','primaryKey','fillable','guarded','modelAttr','selectedCols','whereConditions','joinedTables','groupByConditions','db'];
+	private $selectedCols = '';
+	private $whereConditions = [];
+	private $joinedTables = '';
+	private $groupByConditions = '';
 	private $db;
 	
 	public function __construct()
@@ -20,6 +24,12 @@ abstract class Model extends Db
 		}
 	}
 	
+	/**
+     * Display the generic request
+     * @sql string
+	 * @attributs array=null
+     * @return database response
+     */
 	protected function request(string $sql, array $attributs = null)
 	{
 		$this->db = Db::getInstance();
@@ -77,24 +87,52 @@ abstract class Model extends Db
 						}
 					}
 				}
-				
 			}
 		}
 
 		return $this->save();
 	}
 	
-	//récupérer l'ensemble des entrées d'une table
-	public function all()
-	{
-		$request = $this->request('SELECT * FROM '. $this->table);
-		return $request->fetchAll(PDO::FETCH_OBJ);
+	/*
+	 * get all resources from the database. Can be used with Select, Where, Join functions, Group
+	 * @return object
+	*/
+	public function get(){
+		
+		// traitement des tableaux selected, where, with
+		$selected = (empty($this->selectedCols))?'*':$this->selectedCols;
+		$joined = (empty($this->joinedTables))?'':$this->joinedTables;
+		$grouped = (empty($this->groupByConditions))?'':' GROUP BY '.$this->groupByConditions;
+		
+		if(empty($this->whereConditions)){
+			$where = '';
+			$values = null;
+		}else{
+			$columns = [];
+			$values = [];
+			
+			foreach($this->whereConditions as $condition){
+				$columns[] = $condition[0];
+				$values[] = $condition[1];
+			}
+			
+			$where = ' WHERE '.implode(', ',$columns);
+		}
+
+		$query = 'SELECT '.$selected.' FROM '.$this->table.$joined.$where.$grouped;
+		
+		$request = $this->request($query,$values);
+
+		return $request->fetchAll(PDO::FETCH_CLASS);
 	}
 	
-	//récupérer une entrée d'une table via sa clé primaire
+	//récupérer une entrée d'une table via sa clé primaire (à améliorer comme le get)
 	public function find(int $id)
 	{
-		$request = $this->request('SELECT * FROM '. $this->table .' WHERE '.$this->primaryKey.' = '.$id);
+		$selected = (empty($this->selectedCols))?'*':$this->selectedCols;
+		$joined = (empty($this->joinedTables))?'':$this->joinedTables;
+		
+		$request = $this->request('SELECT '.$selected.' FROM '.$this->table.$joined.' WHERE '.$this->table.'.'.$this->primaryKey.' = '.$id);
 		$request->setFetchMode(PDO::FETCH_INTO, $this);
 		return $request->fetch();
 	}
@@ -121,14 +159,118 @@ abstract class Model extends Db
 	{
 		$key = $this->primaryKey;
 		$id = $this->$key;
-		
+
 		return $this->request('DELETE FROM '.$this->table.' WHERE '.$key.' = ?',[$id]);// on protège cette donnée en provoquant un prepare au lieu d'un query
 	}
 	
-	//supprimer une entrée d'une table via sa clé primaire
-	public function destroy(int $id)
+	/*
+	 * delete a resource by the primary key. Accept an array of primary keys to multiple delete
+	 * @return request
+	*/
+	public function destroy($ids)
 	{	
-		return $this->request('DELETE FROM '.$this->table.' WHERE '.$this->primaryKey.' = ?',[$id]);// on protège cette donnée en provoquant un prepare au lieu d'un query
+		if(is_array($ids)){
+			
+			foreach($ids as $id){
+				$columns[] = '?';
+			}
+			$columns = implode(', ',$columns);
+			$whereIn = 'IN ('.$columns.')';
+			
+		}elseif(is_int($ids)){
+			$whereIn = '= ?';
+			$ids = [$ids];
+		}
+		
+		$query = 'DELETE FROM '.$this->table.' WHERE '.$this->primaryKey.' '.$whereIn;
+		
+		return $this->request($query,$ids);// on protège cette donnée en provoquant un prepare au lieu d'un query
+	}
+	
+	/* Ajouter une sélection d'attributs à la requête
+	 * 
+	 * @return *this
+	 */
+	public function select(...$attributs){
+		
+		$columns = [];
+		foreach($attributs as $attr){
+			$columns[] = $attr;
+		}
+		
+		$columns = implode(', ',$columns);
+		$this->selectedCols = $columns; 
+		
+		return $this;
+	}
+	
+	/**
+	 * Ajouter une condition WHERE à la requête
+	 * 
+	 * @return *this
+	 */
+	public function where(string $attr, string $operator, $value=null){
+		
+		if(!$value){
+			$value = $operator;
+			$operator = '=';
+		}
+		$condition = $attr.' '.$operator.' ?';
+
+		$this->whereConditions[] = [$condition,$value];
+		
+		return $this;
+		
+	}
+	
+		
+	/**
+     * Ajouter un groupement de résultats (GROUP BY) à la requête
+     * 
+     * @return $this
+     */
+	public function groupBy(...$attributs){
+		
+		$columns = [];
+		foreach($attributs as $attr){
+			$columns[] = $attr;
+		}
+		
+		$columns = implode(', ',$columns);
+		$this->groupByConditions = $columns; 
+		
+		return $this;
+
+	}
+	
+	/**
+     * Ajouter une jointure interne (INNER JOIN) à la requête
+     * 
+     * @return $this
+     */
+	public function join(string $joined_table, string $contraint_1,string $operator,string $contraint_2){
+		
+		$query = ' INNER JOIN '.$joined_table.' ON '.$contraint_1.$operator.$contraint_2;
+
+		$this->joinedTables .= $query;
+		
+		return $this;
+		
+	}
+	
+	/**
+     * Ajouter une jointure gauche (LEFT JOIN) à la requête // Séparée de la jointure interne pour plus de clareté
+     * 
+     * @return $this
+     */
+	public function leftJoin(string $joined_table, string $contraint_1,string $operator,string $contraint_2){
+		
+		$query = ' LEFT JOIN '.$joined_table.' ON '.$contraint_1.$operator.$contraint_2;
+
+		$this->joinedTables .= $query;
+		
+		return $this;
+
 	}
 	
 	// retrocompatibilité
