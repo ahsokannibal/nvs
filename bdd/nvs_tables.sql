83da946ac74fc31f21999498b83ecfa34c69fa18
Jacklegende <damien.wattiez1@gmail.com>
2023-11-04 08:54:41 +0100

    merge des differents fichier alter sql pepite train

--- bdd/nvs_tables.sql
+++ bdd/nvs_tables.sql
@@ -1427,6 +1427,9 @@ CREATE TABLE `objet` (
   `type_objet` varchar(3) NOT NULL DEFAULT 'N'
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
+ALTER TABLE `objet` ADD `Perte_Proba` SMALLINT UNSIGNED NOT NULL DEFAULT '0' COMMENT 'valeur entiere de 0 (jamais perdue) a 100 (perdue avec certitude), representant la probabilite de perdre cet objet en cas de rip' AFTER `deposable`;
+ALTER TABLE `objet` CHANGE `type_objet` `type_objet` VARCHAR(3) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL DEFAULT 'N' COMMENT 'T: ticket, E: equipable, N: Consommable, S: soin, SSP: soin special, RP: objet RP';
+
 ALTER TABLE `objet`
   ADD PRIMARY KEY (`id_objet`),
   ADD UNIQUE KEY `index_echangeableTypeObj` (`id_objet`,`type_objet`,`echangeable`),
@@ -2044,7 +2047,8 @@ ALTER TABLE `train_compteur_blocage`
 CREATE TABLE `train_last_dep` (
 	`id_train` INT NOT NULL ,
 	`x_last_dep` INT NOT NULL ,
-	`y_last_dep` INT NOT NULL
+	`y_last_dep` INT NOT NULL,
+  `DeplacementDate` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE = MyISAM;
 
 ALTER TABLE `train_last_dep`
